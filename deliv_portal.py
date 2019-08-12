246d6eee8f43ded4a50005df90eb2a3aed967132
inod5951 <ina.oden.osterbo@gmail.com>
2019-08-12 17:29:52 +0200

    prints user on page. beginning with table.

--- deliv_portal.py
+++ deliv_portal.py
@@ -21,19 +21,36 @@ class BaseHandler(tornado.web.RequestHandler):
 class MainHandler(BaseHandler):
     def get(self):
         # self.current_user = False
-        # if not self.current_user:
-        self.render('home_base.php')
-        #else:
-        #    self.render('home_login.html', user=self.current_user)
+        if not self.current_user:
+            self.render('home_base.php')
+        else:
+            self.render('home_login.html', user=self.current_user)
+
 
 
 class LoginHandler(BaseHandler):
     def post(self):
+        # Get form input
         user_email = self.get_body_argument("user_email")
         password = self.get_body_argument("password")
-        login_connection = pymysql.connect(host="localhost", user="root", password=password, db="del_port_db")
-        # self.set_secure_cookie("user", user_email, expires_days=0)
-        self.redirect(site_base_url + self.reverse_url('home'))
+
+        # Establish database connection
+        login_connection = pymysql.connect(host="localhost",
+                                           user="root",
+                                           password="Polarbear1",
+                                           db="del_port_db")
+
+        # Check if user exists
+        try:
+            with login_connection.cursor() as cursor:   # cursor used to interact with database
+                sql = f"SELECT * FROM `users` WHERE `email`='{user_email}' AND `password`='{password}'"
+                cursor.execute(sql)
+                results = cursor.fetchall()
+                if len(results) == 1:
+                    self.set_secure_cookie("user", user_email, expires_days=0.007)
+                    self.redirect(site_base_url + self.reverse_url('home'))
+        finally:
+            login_connection.close()
 
 
 class CreateDeliveryHandler(BaseHandler):
@@ -45,16 +62,13 @@ class CreateDeliveryHandler(BaseHandler):
         self.render('create_delivery.html')
 
 
-class UserHandler(BaseHandler):
-    def post(self):
-        self.render('login_user.php')
-
-
 def main():
     try:
-        password = "Polarbear1"
         # Open database connection
-        connection = pymysql.connect(host="localhost", user="root", password=password, db="del_port_db")
+        connection = pymysql.connect(host="localhost",
+                                     user="root",
+                                     password="Polarbear1",
+                                     db="del_port_db")
 
         # prepare a cursor object
         cursor = connection.cursor()
@@ -74,9 +88,8 @@ def main():
     url = tornado.web.url
     handlers = [ url(r"/", MainHandler, name='home'),
                  url(r"/login", LoginHandler, name='login'),
-                 url(r"/create", CreateDeliveryHandler, name='create'),
-                 url(r"/user", UserHandler, name='user')
-               ]
+                 url(r"/create", CreateDeliveryHandler, name='create')
+                 ]
 
     # For devel puprose watch page changes
     tornado.autoreload.start()
