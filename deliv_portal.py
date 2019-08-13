52230616b377d305a245b66e813531af3870ad66
inod5951 <ina.oden.osterbo@gmail.com>
2019-08-13 11:30:11 +0200

    file name change, login fix and logout button. All working.

--- deliv_portal.py
+++ deliv_portal.py
@@ -9,9 +9,31 @@ import pymysql
 import tornado_mysql
 
 from utils.config import parse_config
-
 config = parse_config()
 site_base_url = config["site_base_url"]
+
+from tornado.options import define, options
+define("port", default=8888, help="run on the given port", type=int)
+
+
+class ApplicationDP(tornado.web.Application):
+    """docstring for ApplicationDP."""
+
+    def __init__(self):
+        url = tornado.web.url
+        handlers = [ url(r"/", MainHandler, name='home'),
+                     url(r"/login", LoginHandler, name='login'),
+                     url(r"/create", CreateDeliveryHandler, name='create'),
+                     url(r"/logout", LogoutHandler, name='logout')
+                     ]
+        settings = {"xsrf_cookies":True,
+                    #"cookie_secret":base64.b64encode(uuid.uuid4().bytes + uuid.uuid4().bytes),
+                    "cookie_secret":config["cookie_secret"], #for dev purpose, shoulde be removed in the end
+                    "template_path":"html_templates",
+                    "static_path":"files"
+                    }
+        tornado.web.Application.__init__(self, handlers, **settings)
+
 
 class BaseHandler(tornado.web.RequestHandler):
     def get_current_user(self):
@@ -22,18 +44,12 @@ class MainHandler(BaseHandler):
     def get(self):
         # self.current_user = False
         if not self.current_user:
-            self.render('home_base.php')
+            self.render('index.html')
         else:
-            self.render('home_login.html', user=self.current_user)
-
-
+            self.render('home.html', user=self.current_user)
 
 class LoginHandler(BaseHandler):
-    def post(self):
-        # Get form input
-        user_email = self.get_body_argument("user_email")
-        password = self.get_body_argument("password")
-
+    def check_permission(self, username, password):
         # Establish database connection
         login_connection = pymysql.connect(host="localhost",
                                            user="root",
@@ -43,14 +59,39 @@ class LoginHandler(BaseHandler):
         # Check if user exists
         try:
             with login_connection.cursor() as cursor:   # cursor used to interact with database
-                sql = f"SELECT * FROM `users` WHERE `email`='{user_email}' AND `password`='{password}'"
+                sql = f"SELECT * FROM `users` WHERE `email`='{username}' AND `password`='{password}'"
                 cursor.execute(sql)
                 results = cursor.fetchall()
                 if len(results) == 1:
-                    self.set_secure_cookie("user", user_email, expires_days=0.007)
-                    self.redirect(site_base_url + self.reverse_url('home'))
+                    return True
+                else:
+                    return False
         finally:
             login_connection.close()
+
+    def get(self):
+        try:
+            errormessage = self.get_argument("error")
+        except:
+            errormessage = ""
+
+    def post(self):
+        # Get form input
+        user_email = self.get_body_argument("user_email")
+        password = self.get_body_argument("password")
+        auth = self.check_permission(user_email, password)
+
+        if auth:
+            self.set_secure_cookie("user", user_email, expires_days=0.007)
+            self.redirect(site_base_url + self.reverse_url('home'))
+        else:
+            self.clear_cookie("user")
+
+
+class LogoutHandler(BaseHandler):
+    def get(self):
+        self.clear_cookie("user")
+        self.redirect(site_base_url + self.reverse_url('home'))
 
 
 class CreateDeliveryHandler(BaseHandler):
@@ -83,28 +124,14 @@ def main():
         # Disconnect from server
         connection.close()
 
-
-
-    url = tornado.web.url
-    handlers = [ url(r"/", MainHandler, name='home'),
-                 url(r"/login", LoginHandler, name='login'),
-                 url(r"/create", CreateDeliveryHandler, name='create')
-                 ]
-
     # For devel puprose watch page changes
     tornado.autoreload.start()
-    tornado.autoreload.watch("html_templates/home_base.php")
-    tornado.autoreload.watch("html_templates/home_login.html")
+    tornado.autoreload.watch("html_templates/index.html")
+    tornado.autoreload.watch("html_templates/home.html")
     tornado.autoreload.watch("html_templates/create_delivery.html")
-    tornado.autoreload.watch("html_templates/login_user.php")
 
-    application = tornado.web.Application(handlers = handlers,
-                                          xsrf_cookies = True,
-                                          #cookie_secret = base64.b64encode(uuid.uuid4().bytes + uuid.uuid4().bytes),
-                                          cookie_secret = config["cookie_secret"], #for dev purpose, shoulde be removed in the end
-                                          template_path = "html_templates",
-                                          static_path = "files")
-    application.listen(8888)
+    application = ApplicationDP()
+    application.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
