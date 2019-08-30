f8c2156bc4d9b60cdb0112659ac61ce87e8d0157
inod5951 <ina.oden.osterbo@gmail.com>
2019-08-30 17:43:27 +0200

    facility view

--- deliv_portal.py
+++ deliv_portal.py
@@ -15,6 +15,9 @@ import tornado_mysql
 import couchdb
 import re
 
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
 from utils.config import parse_config
 config = parse_config()
 site_base_url = f'{config["site_base_url"]}:{config["site_port"]}'
@@ -35,7 +38,9 @@ class ApplicationDP(tornado.web.Application):
                      url(r"/create", CreateDeliveryHandler, name='create'),
                      url(r"/logout", LogoutHandler, name='logout'),
                      url(r"/project/(?P<projid>.*)", ProjectHandler, name='project'),
-                     url(r"/profile", ProfileHandler, name='profile')
+                     url(r"/profile", ProfileHandler, name='profile'),
+                     url(r"/info", InfoHandler, name='info'),
+                     url(r"/contact", ContactHandler, name="contact")
                      # url(r"/files/(?P<pid>.*)", FileHandler, name='files')
                      ]
         settings = {"xsrf_cookies":True,
@@ -53,6 +58,67 @@ class ApplicationDP(tornado.web.Application):
         tornado.web.Application.__init__(self, handlers, **settings)
 
 
+class AESKey:
+    """"""
+
+    def __init__(self, bytes=32):
+        """
+        :param bytes:
+        """
+
+        self.key = get_random_bytes(bytes)   # Switch to better method? More randomness? os.urandom()?
+
+    def encrypt(self, id, extn="", file=False):
+        """
+        :param data:
+        :param file:        True if the data is a file.
+        :return:
+        """
+
+        if file:
+            with open("{}.txt".format(id), 'rb') as f:
+                with open("encrypted_{}{}".format(id, extn), 'wb') as enc_f:
+                    while True:
+                        cipher = AES.new(self.key, AES.MODE_GCM)                # Create cipher
+                        plaintext = f.read(61440)                               # Read 60 KiB
+                        if not plaintext:                                       # Stop encryption if nothing to encrypt
+                            break
+                        ciphertext, tag = cipher.encrypt_and_digest(plaintext)  # Encrypt and extract tag
+                        enc_f.write(cipher.nonce + tag + ciphertext)            # Save nonce, tag and ciphertext
+            return "OK"
+        else:
+            cipher = AES.new(self.key, AES.MODE_GCM)                # Create cipher
+            ciphertext, tag = cipher.encrypt_and_digest(id)
+            return ciphertext, tag, cipher.nonce
+
+
+    def decrypt(self, id, extn, file=False, iv=b'', tag=b''):
+        """
+        :param data:
+        :param file:    True if data is a file.
+        :return:
+        """
+
+        if file:
+            with open('encrypted_downloaded_{}{}'.format(id, extn), 'rb') as enc_f:
+                with open('decrypted_{}{}'.format(id, extn), 'wb') as dec_f:
+                    while True:
+                        nonce = enc_f.read(16)
+                        mac = enc_f.read(16)
+                        if not nonce and not mac:
+                            break
+                        cipher = AES.new(self.key, AES.MODE_GCM, nonce)
+                        ciphertext = enc_f.read(61440)
+
+                        plaintext = cipher.decrypt_and_verify(ciphertext, mac)
+                        dec_f.write(plaintext)
+            return "OK"
+        else:
+            cipher = AES.new(self.key, AES.MODE_GCM, iv)
+            plaintext = cipher.decrypt_and_verify(id, tag)
+            return plaintext
+
+
 class BaseHandler(tornado.web.RequestHandler):
     """docstring for BaseHandler"""
     def get_current_user(self):
@@ -66,26 +132,36 @@ class BaseHandler(tornado.web.RequestHandler):
         return couch
 
 
+class ContactHandler(BaseHandler):
+    """"""
+
+    def get(self):
+        message = "This is the page where contact info is displayed. "
+        self.render("contact_page.html", user=self.current_user, message=message)
+
+
 class CreateDeliveryHandler(BaseHandler):
     """Called by create button on home page.
     Renders form for a new delivery. """
 
     def get(self):
         """Renders form for a new delivery."""
-        self.render('create_delivery.html', pid="dgu8y3488hdfs8dh88r3")
+        self.render('create_delivery.html', user=self.current_user,
+                    pid="dgu8y3488hdfs8dh88r3")
 
     #@tornado.web.authenticated
     # def post(self):
     #     """"""
     #     self.render('create_delivery.html')
+    #
 
 
-class FileHandler(BaseHandler):
-    """docstring for FileHandler"""
-    def get(self, pid):
-        """"""
-        self.render('view_all.html')
+class InfoHandler(BaseHandler):
+    """ """
 
+    def get(self):
+        message = "This is an information page about the dp."
+        self.render("info_dp.html", user=self.current_user, message=message)
 
 class LoginHandler(BaseHandler):
     """ Handles request to log in user. """
@@ -154,8 +230,15 @@ class MainHandler(BaseHandler):
         else:
             # Get projects associated with user and send to home page
             # with user and project info
-            projects, email = self.get_user_projects()
-            self.render('home.html', user=self.current_user, email=email,
+            projects, email, is_facility = self.get_user_projects()
+
+            homepage=""
+            if is_facility:
+                homepage = "facility_home.html"
+            else:
+                homepage = "home.html"
+
+            self.render(homepage, user=self.current_user, email=email,
                         projects=projects)
 
     def get_user_projects(self):
@@ -174,20 +257,35 @@ class MainHandler(BaseHandler):
             for proj in user_db[user]['projects']:
                 projects[proj] = proj_db[proj]['project_info']
 
-        return projects, user_db[user]['user']['email']
+        return projects, user_db[user]['user']['email'], ("facility" in user_db[user]["user"])
 
 
-class ProfileHandler():
+class ProfileHandler(BaseHandler):
     """Profile page."""
 
     def get(self):
         message="This is the profile page where one can change password etc. "
-        self.render('profile.html', message=message)
+
+        self.render('profile.html', user=self.current_user, message=message)
+
 
 class ProjectHandler(BaseHandler):
     """Called by "See project" button.
     Connects to database and collects all files
     associated with the project and user. Renders project page."""
+
+    def post(self, projid):
+        """"""
+        couch = self.couch_connect()
+
+        proj_name = self.get_body_argument('prj_name')
+        proj_category = self.get_body_argument('prj_ord_cat')
+        proj_id = self.get_body_argument('prj_ord_id')
+        proj_description = self.get_body_argument('prj_desc')
+
+        pi_name = self.get_body_argument('prj_pi_name')
+        pi_email = self.get_body_argument('prj_pi_email')
+
 
     def get(self, projid):
         """"""
@@ -204,29 +302,149 @@ class ProjectHandler(BaseHandler):
                     files=files, project=project_info)
 
 
+@tornado.web.stream_request_body        # Allows for uploading of large files
+class UploadHandler(BaseHandler):
+    """Class. Handles the upload of the file."""
+
+    def initialize(self):
+        """
+        Initialized the file upload.
+        :return: ----
+        """
+
+        self.bytes_read = 0
+        self.data = b''
+        self.id = str(uuid.uuid4())    # Unique id -- Bucket name
+        self.aes_key = AESKey()
+        self.bucket = "s3://kMTZXkjLvM47TFn11zjhHJ8UlkT8PxrS"
+
+    def prepare(self):
+        """
+        Sets the max streamed size.
+        :return: ----
+        """
+
+        self.request.connection.set_max_body_size(MAX_STREAMED_SIZE)
+
+    def data_received(self, chunk):
+        """
+        Received the data.
+        :param chunk:   chunk of data
+        :return:        ----
+        """
+
+        self.bytes_read += len(chunk)
+        self.data += chunk
+
+    def post(self):
+        """
+        Saves the uploaded data and checks that the file is identical to the uploaded file.
+        :return: ---
+        """
+
+        # this_request gives HTTPServerRequest(protocol='', host='', method='', uri='', version='', remote_ip='')
+        this_request = self.request
+        value = self.data       # Not really useful, self.data could just be used directly
+
+        # Save uploaded file
+        with open(self.id + ".txt", 'wb') as f:
+            f.write(value)
+
+        # Get file name and extension from header
+        header_cmd = subprocess.Popen(['sed', '2!d', self.id + ".txt"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        file_name_orig = header_cmd.communicate()[0].decode().split(";")[2].split('"')[1]
+        file_extn = "." + file_name_orig.split(".")[-1]
+
+        # Delete header and tail from file
+        print("[*]  Removing header and tail from file...")
+        remove_header = subprocess.Popen(["sed", "-i", ".bak", "1,4d;$d", self.id + ".txt"],
+                                         stdout=subprocess.PIPE, stderr=subprocess.PIPE).wait()
+        remove_tail = subprocess.Popen(["sed", "-i", ".bak", "$d", self.id + ".txt"],
+                                       stdout=subprocess.PIPE, stderr=subprocess.PIPE).wait()
+        os.remove(self.id + ".txt.bak")
+        print("[*]  Header and tail removed.")
+
+        # Generate, print and compare checksums
+        print("[*]  Generating hashes and checking integrity...")
+        hash_upload = hash_file(self.id + ".txt")
+        hash_orig = hash_file('/Users/inaod568/Downloads/{}'.format(file_name_orig))
+        print("[*]  The files are identical: ", (hash_orig == hash_upload))
+
+        if hash_orig == hash_upload:
+            # Encrypt file
+            print("[*]  Beginning encryption...")
+            file = True
+            file_encrypted = self.aes_key.encrypt(self.id, file_extn, file=file)
+            print("[*]  Encryption completed.")
+
+            if file==True and file_encrypted == "OK":
+                try:
+                    os.remove(self.id + ".txt")
+                except IOError:
+                    print(sys.exc_info())
+
+                # Get size of original and encrypted file:
+                print("[*]  Calculating overhead...")
+                file_size_orig = os.path.getsize('/Users/inaod568/Downloads/{}'.format(file_name_orig))
+                file_size_upload = os.path.getsize('encrypted_{}{}'.format(self.id, file_extn))
+                print("Original file size: {} MB, \t "
+                      "Encrypted file size: {} MB, \n "
+                      "[*]  Overhead: {} %".format(round(file_size_orig / 1e6, 3),
+                                                   round(file_size_upload / 1e6, 3),
+                                                   round((file_size_upload - file_size_orig) / file_size_orig, 3)))
+
+            # Create bucket if bucket doesn't exist
+            buckets = subprocess.Popen([options.s3, "ls"], stdout=subprocess.PIPE,
+                                             stderr=subprocess.PIPE).communicate()[0].decode().split("\n")
+            bucket_exists = False
+            for buck in buckets:
+                if buck.split(" ")[-1] == self.bucket:
+                    bucket_exists = True
+
+            if not bucket_exists:
+                create_bucket = subprocess.Popen([options.s3, "mb", self.bucket],
+                                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE).wait()
+
+            # Put uploaded file in bucket
+            print("[*]  Uploading file to bucket...")
+            file_to_bucket = subprocess.Popen(['{}'.format(options.s3),     # s3cmd directory
+                                               'put',                       # put = upload file to bucket
+                                               'encrypted_{}{}'.format(self.id, file_extn),      # file name
+                                               self.bucket],
+                                              stdout=subprocess.PIPE, stderr=subprocess.PIPE).wait()
+            print("[*]  File upload completed: encrypted_{}{}".format(self.id, file_extn))
+
+            try:
+                os.remove('encrypted_{}{}'.format(self.id, file_extn))
+            except IOError:
+                print(sys.exc_info())
+
+            print("[*]  Beginning download...")
+            file_from_bucket = subprocess.Popen(['{}'.format(options.s3),
+                                                 'get',
+                                                 '{}/encrypted_{}{}'.format(self.bucket, self.id, file_extn),
+                                                 'encrypted_downloaded_{}{}'.format(self.id, file_extn)]).communicate()
+            print("[*]  Download completed: encrypted_downloaded_{}{}".format(self.id, file_extn))
+
+            print("[*]  Beginning decryption...")
+            file_decrypted = self.aes_key.decrypt(self.id, file_extn, file=True)
+            print("[*]  Decryption completed. File name: decrypted_{}{}".format(self.id, file_extn))
+
+            if file_decrypted == "OK":
+                os.remove('encrypted_downloaded_{}{}'.format(self.id, file_extn))
+
+                print("[*]  Generating hashes and verifying integrity...")
+                hash_download = hash_file('decrypted_{}{}'.format(self.id, file_extn))
+                if hash_upload == hash_download:
+                    print("[*]  SUCCESS!")
+
+        return self.write('<input onClick="history.back()" type="button" value="Back" />')
+
+
+
+
+
 # FUNCTIONS ######################################################## FUNCTIONS #
-def test_db_connection():
-    """Tests connection to database"""
-    try:
-        # Open database connection
-        connection = pymysql.connect(host="localhost",
-                                     user="root",
-                                     password="Polarbear1",
-                                     db="del_port_db")
-
-        # prepare a cursor object
-        cursor = connection.cursor()
-
-        # Execute sql query: What is the database version?
-        cursor.execute("SELECT VERSION()")
-
-        # Fetch a single row
-        data = cursor.fetchone()
-        print(f"Database version: {data}")
-    finally:
-        # Disconnect from server
-        connection.close()
-
 
 # MAIN ################################################################## MAIN #
 def main():
@@ -238,9 +456,11 @@ def main():
         tornado.autoreload.start()
         tornado.autoreload.watch("html_templates/index.html")
         tornado.autoreload.watch("html_templates/home.html")
-        tornado.autoreload.watch("html_templates/create_delivery.html")
         tornado.autoreload.watch("html_templates/project_page.html")
         tornado.autoreload.watch("html_templates/style.css")
+        tornado.autoreload.watch("html_templates/profile.html")
+        tornado.autoreload.watch("html_templates/info_dp.html")
+        tornado.autoreload.watch("html_templates/contact_page.html")
 
     application = ApplicationDP()
     application.listen(options.port)
