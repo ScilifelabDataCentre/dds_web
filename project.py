fb34540098b8e156149bfa1d55f4d1c1e2bb6eff
inod5951 <ina.oden.osterbo@gmail.com>
2019-09-11 16:26:32 +0200

    del post in projecthandler (moved)

--- project.py
+++ project.py
@@ -46,32 +46,6 @@ class ProjectHandler(BaseHandler):
     Connects to database and collects all files
     associated with the project and user. Renders project page."""
 
-    def post(self, projid):
-        """Sets project status to finished or open depending on which
-        button is pressed on project page."""
-
-        if ((self.get_argument('setasfinished', None) is not None)
-                or (self.get_argument('setasopen', None) is not None)):
-            couch = self.couch_connect()
-
-            proj_db = couch['projects']
-            curr_proj = proj_db[projid]
-
-            if self.get_argument('setasfinished', None) is not None:
-                curr_proj['project_info']['status'] = "Uploaded"
-            elif self.get_argument('setasopen', None) is not None:
-                curr_proj['project_info']['status'] = "Delivery in progress"
-
-            try:
-                proj_db.save(curr_proj)
-            finally:
-                self.render('project_page.html',
-                            curr_user=self.current_user,
-                            projid=projid,
-                            curr_project=curr_proj['project_info'],
-                            files=curr_proj['files'],
-                            addfiles=(self.get_argument('uploadfiles', None) is not None))
-
     def get(self, projid):
         """Renders the project page with projects and associated files."""
 
