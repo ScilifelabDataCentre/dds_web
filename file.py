5cda3f6248221b9e7a737b6fea67a45b3916e42b
inod5951 <ina.oden.osterbo@gmail.com>
2019-09-20 10:11:49 +0200

    js packages and comments minimal changes bc of failed upload attempts

--- file.py
+++ file.py
@@ -7,7 +7,8 @@
 from __future__ import absolute_import
 import sys
 from datetime import date
-from tornado.web import stream_request_body
+
+
 
 from base import BaseHandler
 
@@ -18,50 +19,50 @@ MAX_STREAMED_SIZE = 1024 * 1024 * 1024
 
 # CLASSES ############################################################ CLASSES #
 
-# class UploadHandler(BaseHandler):
-#     """Class. Handles the upload of the file."""
-#
-#     def post(self, projid):
-#         """post"""
-#
-#         # Checks if there are files "uploaded"
-#         files = []
-#         try:
-#             files = self.request.files['filesToUpload']
-#         except OSError:
-#             pass
-#
-#         # Connects to the database
-#         couch = self.couch_connect()            # couchdb
-#         proj_db = couch['projects']             # database: projects
-#         curr_proj = proj_db[projid]             # current project
-#         curr_proj_files = curr_proj['files']    # files assoc. with project
-#
-#         # Save files (now uploaded)
-#         for file_ in files:
-#             filename = file_['filename']
-#
-#             try:
-#                 with open(filename, "wb") as out:
-#                     out.write(file_['body'])
-#             finally:
-#                 curr_proj_files[filename] = {
-#                     "size": sys.getsizeof(filename),
-#                     "format": filename.split(".")[-1],
-#                     "date_uploaded": date.today().strftime("%Y-%m-%d"),
-#                 }
-#
-#         # Save couchdb --> updated
-#         # and show the project page again.
-#         try:
-#             proj_db.save(curr_proj)
-#         finally:
-#             self.render('project_page.html',
-#                         curr_user=self.current_user,
-#                         projid=projid,
-#                         curr_project=curr_proj['project_info'],
-#                         files=curr_proj_files,
-#                         addfiles=(self.get_argument('uploadfiles', None) is not None))
+class UploadHandler(BaseHandler):
+    """Class. Handles the upload of the file."""
+
+    def post(self, projid):
+        """post"""
+
+        # Checks if there are files "uploaded"
+        files = []
+        try:
+            files = self.request.files['filesToUpload']
+        except OSError:
+            pass
+
+        # Connects to the database
+        couch = self.couch_connect()            # couchdb
+        proj_db = couch['projects']             # database: projects
+        curr_proj = proj_db[projid]             # current project
+        curr_proj_files = curr_proj['files']    # files assoc. with project
+
+        # Save files (now uploaded)
+        for file_ in files:
+            filename = file_['filename']
+
+            try:
+                with open(filename, "wb") as out:
+                    out.write(file_['body'])
+            finally:
+                curr_proj_files[filename] = {
+                    "size": sys.getsizeof(filename),
+                    "format": filename.split(".")[-1],
+                    "date_uploaded": date.today().strftime("%Y-%m-%d"),
+                }
+
+        # Save couchdb --> updated
+        # and show the project page again.
+        try:
+            proj_db.save(curr_proj)
+        finally:
+            self.render('project_page.html',
+                        curr_user=self.current_user,
+                        projid=projid,
+                        curr_project=curr_proj['project_info'],
+                        files=curr_proj_files,
+                        addfiles=(self.get_argument('uploadfiles', None) is not None))
 
 # stream_request_body -- apply to RequestHandler subclasses to enable streaming body support
 # HTTPServerRequest.body = undefined
@@ -73,28 +74,30 @@ MAX_STREAMED_SIZE = 1024 * 1024 * 1024
 # prepare & data_received = may return Futures --> next method not called until
 #                           those futures have been completed
 # The regular HTTP method will be called after the entire body has been read
-@stream_request_body
-class UploadHandler(BaseHandler):
-    """docstring"""
-
-    # Hook for subclass initialization. Called for each request.
-    def initialize(self):
-        """docstring"""
-        self.bytes_read = 0
-        self.data = b''
-
-    # Called at the beginning of a request before get/post/etc.
-    def prepare(self):
-        self.request.connection.set_max_body_size(MAX_STREAMED_SIZE)
-
-    # Implement this method to handle streamed request data.
-    def data_received(self, chunk):
-        self.bytes_read += len(chunk)
-        self.data += chunk
-
-    def post(self, projid):
-        """docstring"""
-        this_request = self.request
-        with open('file', 'w') as f:
-            for h in self.request.headers:
-                f.write(f"{h}\n")
+# @stream_request_body
+# class UploadHandler(BaseHandler):
+#     """docstring"""
+#
+#     # Hook for subclass initialization. Called for each request.
+#     def initialize(self):
+#         """docstring"""
+#         self.bytes_read = 0
+#         self.data = b''
+#
+#     # Called at the beginning of a request before get/post/etc.
+#     def prepare(self):
+#         self.request.connection.set_max_body_size(MAX_STREAMED_SIZE)
+#         self.boundary = self.request.headers['Content-Type'].split('boundary=')[-1]
+#
+#     # Implement this method to handle streamed request data.
+#     def data_received(self, chunk):
+#         self.bytes_read += len(chunk)
+#         self.data += chunk
+#         with open('boundaryfile', 'w') as f:
+#             f.write(f"{self.data} \t {self.boundary} \n")
+#
+#     def post(self, projid):
+#         """docstring"""
+#         this_request = self.request
+#         with open('file', 'w') as f:
+#             f.write(f"{self.boundary} \n")
