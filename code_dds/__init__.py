9665af361aa257f31387ff7339e846bff1e88cdc
inaod568 <ina.oden.osterbo@gmail.com>
2020-09-23 11:27:40 +0200

    sqlalchemy

--- code_dds/__init__.py
+++ code_dds/__init__.py
@@ -1,43 +1,63 @@
-"Web app template based on Flask and Sqlite3. With user account handling."
+# "Web app template based on Flask and Sqlite3. With user account handling."
 
-import re
+# import re
 
-__version__ = "0.0.2"
-
-
-class Constants:
-    VERSION = __version__
-    SOURCE_NAME = ""
-    SOURCE_URL = ""
-
-    BOOTSTRAP_VERSION = "4.3.1"
-    JQUERY_VERSION = "3.3.1"
-    DATATABLES_VERSION = "1.10.18"
-
-    NAME_RX = re.compile(r"^[a-z][a-z0-9_-]*$", re.I)
-    IUID_RX = re.compile(r"^[a-f0-9]{32,32}$", re.I)
-    EMAIL_RX = re.compile(r"^[a-z0-9_.+-]+@[a-z0-9-]+\.[a-z0-9-.]+$")
-
-    # User roles
-    ADMIN = "admin"
-    USER = "user"
-    USER_ROLES = (ADMIN, USER)
-
-    # User statuses
-    PENDING = "pending"
-    ENABLED = "enabled"
-    DISABLED = "disabled"
-    USER_STATUSES = [PENDING, ENABLED, DISABLED]
-
-    # Content types
-    HTML_MIMETYPE = "text/html"
-    JSON_MIMETYPE = "application/json"
-
-    # Misc
-    JSON_SCHEMA_URL = "http://json-schema.org/draft-07/schema#"
-
-    def __setattr__(self, key, value):
-        raise ValueError("cannot set constant")
+# __version__ = "0.0.2"
 
 
-constants = Constants()
+# class Constants:
+#     VERSION = __version__
+#     SOURCE_NAME = ""
+#     SOURCE_URL = ""
+
+#     BOOTSTRAP_VERSION = "4.3.1"
+#     JQUERY_VERSION = "3.3.1"
+#     DATATABLES_VERSION = "1.10.18"
+
+#     NAME_RX = re.compile(r"^[a-z][a-z0-9_-]*$", re.I)
+#     IUID_RX = re.compile(r"^[a-f0-9]{32,32}$", re.I)
+#     EMAIL_RX = re.compile(r"^[a-z0-9_.+-]+@[a-z0-9-]+\.[a-z0-9-.]+$")
+
+#     # User roles
+#     ADMIN = "admin"
+#     USER = "user"
+#     USER_ROLES = (ADMIN, USER)
+
+#     # User statuses
+#     PENDING = "pending"
+#     ENABLED = "enabled"
+#     DISABLED = "disabled"
+#     USER_STATUSES = [PENDING, ENABLED, DISABLED]
+
+#     # Content types
+#     HTML_MIMETYPE = "text/html"
+#     JSON_MIMETYPE = "application/json"
+
+#     # Misc
+#     JSON_SCHEMA_URL = "http://json-schema.org/draft-07/schema#"
+
+#     def __setattr__(self, key, value):
+#         raise ValueError("cannot set constant")
+
+
+# constants = Constants()
+
+"""Initialize Flask app."""
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def create_app():
+    """Construct the core application."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
+
+    db.init_app(app)
+
+    with app.app_context():
+        from . import routes  # Import routes
+        db.create_all()  # Create database tables for our data models
+
+        return app
--- code_dds/app.py
+++ code_dds/app.py
@@ -1,77 +1,85 @@
-"Web app template."
+# "Web app template."
 
-# IMPORTS ########################################################### IMPORTS #
+# # IMPORTS ########################################################### IMPORTS #
 
-# Standard library
+# # Standard library
 
-# Installed
-from flask import (Flask, g, redirect, render_template,
-                   request, url_for, flash)
-from flask_restful import Api, Resource
-import jinja2
-import mariadb
-import logging
-# Own modules
-from code_dds import constants
-from code_dds import utils
+# # Installed
+# from flask import (Flask, g, redirect, render_template,
+#                    request, url_for, flash)
+# from flask_restful import Api, Resource
+# import jinja2
+# import mariadb
+# import logging
+# # Own modules
+# from code_dds import constants
+# from code_dds import utils
 
-from code_dds import user
-from code_dds import config
+# from code_dds import user
+# from code_dds import config
 
-from code_dds.api import api_blueprint
+# from code_dds.api import api_blueprint
 
-# CONFIG ############################################################# CONFIG #
+# # CONFIG ############################################################# CONFIG #
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-# URL map converters - "xxx" will result in that xxx can be used in @app.route
-app.url_map.converters["name"] = utils.NameConverter
-app.url_map.converters["iuid"] = utils.IuidConverter
+# # URL map converters - "xxx" will result in that xxx can be used in @app.route
+# app.url_map.converters["name"] = utils.NameConverter
+# app.url_map.converters["iuid"] = utils.IuidConverter
 
-# Get and initialize app configuration
-config.init(app)
+# # Get and initialize app configuration
+# config.init(app)
 
-# Add template filters - "converts" integers with thousands delimiters
-app.add_template_filter(utils.thousands)
-
-
-# add_resource(resource, *urls, **kwargs)
-# endpoint -- endpoint name --> can be used to reference the route in fields.Url fields
-# add_resource(HelloWorld, "/helloworld/<string:name>")
-app.register_blueprint(api_blueprint, url_prefix='/api/v1')
-
-# Context processors injects new variables automatically into the context of a
-# template. Runs before the template is rendered.
-# Returns a dictionary. Keys and values are merged with the template context
-# for all templates in the app. In this case: the constants and the function
-# csrf_token.
+# # Add template filters - "converts" integers with thousands delimiters
+# app.add_template_filter(utils.thousands)
 
 
-@app.context_processor
-def setup_template_context():
-    "Add useful stuff to the global context of Jinja2 templates."
-    return dict(constants=constants,
-                csrf_token=utils.csrf_token)
+# # add_resource(resource, *urls, **kwargs)
+# # endpoint -- endpoint name --> can be used to reference the route in fields.Url fields
+# # add_resource(HelloWorld, "/helloworld/<string:name>")
+# app.register_blueprint(api_blueprint, url_prefix='/api/v1')
+
+# # Context processors injects new variables automatically into the context of a
+# # template. Runs before the template is rendered.
+# # Returns a dictionary. Keys and values are merged with the template context
+# # for all templates in the app. In this case: the constants and the function
+# # csrf_token.
 
 
-# Registers a function to run before >>each<< request
-@app.before_request
-def prepare():
-    "Open the database connection; get the current user."
-    g.db = mariadb.connect(**app.config['DB'])
-    g.current_user = "tester"
+# @app.context_processor
+# def setup_template_context():
+#     "Add useful stuff to the global context of Jinja2 templates."
+#     return dict(constants=constants,
+#                 csrf_token=utils.csrf_token)
 
 
-# app.after_request(utils.log_access)
+# # Registers a function to run before >>each<< request
+# @app.before_request
+# def prepare():
+#     "Open the database connection; get the current user."
+#     g.db = mariadb.connect(**app.config['DB'])
+#     g.current_user = "tester"
 
 
-@app.route("/")
-def home():
-    """Home page."""
-    return render_template("home.html")
+# # app.after_request(utils.log_access)
 
 
-# This code is used only during development.
+# @app.route("/")
+# def home():
+#     """Home page."""
+#     return render_template("home.html")
+
+
+# # This code is used only during development.
+# if __name__ == "__main__":
+#     app.run(host=app.config["SERVER_HOST"],
+#             port=app.config["SERVER_PORT"])
+
+"""App entry point."""
+from code_dds import create_app
+
+app = create_app()
+
 if __name__ == "__main__":
-    app.run(host=app.config["SERVER_HOST"],
-            port=app.config["SERVER_PORT"])
+    app.run(host='0.0.0.0', port=5000)
