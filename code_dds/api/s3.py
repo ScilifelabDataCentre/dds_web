9bb2b6c195843f4504ebb8e397bc022f4da997a5
inaod568 <ina.oden.osterbo@gmail.com>
2020-11-02 11:35:13 +0100

    s3 documentation and pylint fixes

--- code_dds/api/s3.py
+++ code_dds/api/s3.py
@@ -1,20 +1,44 @@
-from flask_restful import Resource
-from code_dds.models import S3Project
-from code_dds.marshmallows import s3_schema, s3s_schema
-import pathlib
+"""S3-related API endpoints."""
+
+###############################################################################
+# IMPORTS ########################################################### IMPORTS #
+###############################################################################
+
+# Standard library
 import json
+import pathlib
+
+# Installed
+import flask_restful
+
+# Own modules
+from code_dds import marshmallows as marmal
+from code_dds import models
 
 
-class ListS3(Resource):
+###############################################################################
+# ENDPOINTS ####################################################### ENDPOINTS #
+###############################################################################
+
+class ListS3(flask_restful.Resource):
+    """Endpoint for listing all S3 projects in the database."""
+
     def get(self):
-        all_s3projects = S3Project.query.all()
-        return s3s_schema.dump(all_s3projects)
+        """Gets S3 projects from database and returns in request response."""
+
+        all_s3projects = models.S3Project.query.all()
+        return marmal.s3s_schema.dump(all_s3projects)
 
 
-class S3Info(Resource):
+class S3Info(flask_restful.Resource):
+    """Endpoint for getting S3 connection information."""
+
     def get(self):
+        """Gets the information from file (atm, will be changed) and returns
+        json response."""
+
         s3path = pathlib.Path.cwd() / \
-                pathlib.Path("sensitive/s3_config.json")
+            pathlib.Path("sensitive/s3_config.json")
         with s3path.open(mode="r") as f:
             s3creds = json.load(f)
 
