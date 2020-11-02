07ad9bb7d4887c51d721d465a9ac01ed5cc3892e
inaod568 <ina.oden.osterbo@gmail.com>
2020-11-02 11:47:16 +0100

    user documentation and pylint fixes

--- code_dds/api/facility.py
+++ code_dds/api/facility.py
@@ -16,10 +16,10 @@ from code_dds import marshmallows as marmal
 from code_dds import models
 from code_dds.api import login
 
-###############################################################################
-# ENDPOINT ######################################################### ENDPOINT #
-###############################################################################
 
+###############################################################################
+# ENDPOINTS ####################################################### ENDPOINTS #
+###############################################################################
 
 class LoginFacility(flask_restful.Resource):
     """Handles the access checks of the facility."""
@@ -100,7 +100,7 @@ class ListFacilities(flask_restful.Resource):
     """Lists all facilities in database."""
 
     def get(self):
-        """Get all facilities from db and return them in response."""
+        """Gets all facilities from db and return them in response."""
 
         all_facilities = models.Facility.query.all()
         return marmal.facs_schema.dump(all_facilities)
