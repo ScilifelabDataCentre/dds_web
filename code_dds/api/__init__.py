bb36d5a458d1d0c7050eff5ab51c8afb8d8f97cc
inaod568 <ina.oden.osterbo@gmail.com>
2020-11-02 09:30:14 +0100

    pylint fixes and documentation

--- code_dds/api/__init__.py
+++ code_dds/api/__init__.py
@@ -1,6 +1,8 @@
 """REST API for the Data Delivery System"""
 
+###############################################################################
 # IMPORTS ########################################################### IMPORTS #
+###############################################################################
 
 # Standard library
 
@@ -17,32 +19,38 @@ from code_dds.api.project import (ProjectFiles, ListProjects,
 from code_dds.api.files import ListFiles, FileSalt, DeliveryDate, FileUpdate
 from code_dds.api.s3 import ListS3, S3Info
 
+
+###############################################################################
+# BLUEPRINTS ##################################################### BLUEPRINTS #
+###############################################################################
+
 api_blueprint = Blueprint('api_blueprint', __name__)
 api = Api(api_blueprint)
 
 
+###############################################################################
+# RESOURCES ####################################################### RESOURCES #
+###############################################################################
+
 # Login/access
-# api.add_resource(PasswordSettings,
-#  '/pw_settings/<string:role>/<string:username>', endpoint='pw_settings')
 api.add_resource(LoginFacility, '/fac/login', endpoint='f_login')
 api.add_resource(LoginUser, '/user/login', endpoint='u_login')
-
-# api.add_resource(LogoutUser, '/user/logout', endpoint='u_logout')
-# api.add_resource(LogoutFacility, '/fac/logout', endpoint='f_logout')
 
 # List
 api.add_resource(ListUsers, '/listusers', endpoint='list_users')
 api.add_resource(ListFacilities, '/listfacs', endpoint='list_facs')
-api.add_resource(
-    ProjectFiles, '/project/listfiles/<int:project>/<string:token>', endpoint='project_files')
+api.add_resource(ProjectFiles,
+                 '/project/listfiles/<int:project>/<string:token>',
+                 endpoint='project_files')
 api.add_resource(ListFiles, '/listfiles', endpoint='list_files')
 api.add_resource(ListS3, '/lists3', endpoint='list_s3')
 
 # Delivery
 api.add_resource(FileUpdate, '/project/updatefile', endpoint='update_file')
 api.add_resource(FileSalt, '/file/salt/<int:file_id>', endpoint='file_salt')
-
-api.add_resource(ProjectKey, '/project/<int:project>/key/<string:token>', endpoint='key')
 api.add_resource(DeliveryDate, '/delivery/date/', endpoint='delivery_date')
-
 api.add_resource(S3Info, '/s3info', endpoint="s3info")
+
+# Key
+api.add_resource(ProjectKey, '/project/<int:project>/key/<string:token>',
+                 endpoint='key')
