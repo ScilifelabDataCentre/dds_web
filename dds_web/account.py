5836674a81c289b152f51d29cb8fcc64a325c28a
Pernilla Ericsson <pernilla.ericsson@scilifelab.se>
2021-06-07 13:58:08 +0200

    started function for printing account info

---
+++ dds_web/account.py
@@ -0,0 +1,34 @@
+" Account info related endpoints "
+
+from flask import Blueprint, render_template, request, current_app, session, redirect, url_for
+
+from dds_web import timestamp
+from dds_web.database import db_utils
+from dds_web.utils import login_required
+
+# temp will be removed in next version
+from dds_web.development import cache_temp as tc
+
+account_blueprint = Blueprint("account", __name__)
+
+@account_blueprint.route("/<loginname>_account", methods=["GET"])
+@login_required
+def account_info(loginname=None):
+    """account page"""
+    #return session
+    if session.get("is_admin"):
+        return "admin"
+        # return redirect(url_for("admin.admin_page"))
+    if session["is_facility"]:
+        return "facility"
+        # projects_list = db_utils.get_facilty_projects(fid=session["facility_id"])
+    else:
+        return "other"
+        # projects_list = db_utils.get_user_projects(uid=session["current_user_id"])
+    # TO DO: change dbfunc passing in future
+    # return render_template(
+    #     "project/list_project.html",
+    #     projects_list=projects_list,
+    #     dbfunc=db_utils.get_facility_column,
+    #     timestamp=timestamp,
+    # )
