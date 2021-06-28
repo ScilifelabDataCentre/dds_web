e82b6069a47b87abe19f02fafca4e392f26338e9
Pernilla Ericsson <pernilla.ericsson@scilifelab.se>
2021-06-28 15:16:07 +0200

    tested another structure for hwne data is in the db

--- dds_web/account.py
+++ dds_web/account.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, current_app, session, redirect, url_for,json
+from flask import Blueprint, render_template, request, current_app, session, redirect, url_for
 
 from dds_web import timestamp
 from dds_web.api.login import ds_access
@@ -26,45 +26,60 @@ def account_test(loginname=None):
 
     username=session["current_user"]
 
-    account_info = {
-            'username': username,
-            'emails': [],
-            'permissions': None,
-            'first_name': None,
-            'last_name': None
-        }
+    account_info = {}
+    account_info['username'] = username
+    account_info['permissions'] = db_utils.get_user_column_by_username(username, 'permissions')
+    account_info['first_name'] = None #db_utils.get_user_column_by_username(username, 'first_name')
+    account_info['last_name'] =  None #db_utils.get_user_column_by_username(username, 'last_name')
 
-    for info in account_info:
-        if info != "username" or info !="emails":
-            try:
-                account_info[info] = db_utils.get_user_column_by_username(username, info)
-            except:
-                pass
-        if info == "emails":
-            user_info_list = models.User.query.filter_by(username=username).all()
-            account_info["emails"] = [
-                {'address': getattr(user_row, "email", None),
-                'primary': getattr(user_row, "primary", False)}
-                for user_row in user_info_list
-            ]
-
+    user_info_list = models.User.query.filter_by(username=username).all()
+    account_info["emails"] = [
+        {'address': getattr(user_row, "email", None),
+        'primary': getattr(user_row, "primary", False)}
+        for user_row in user_info_list
+    ]
     account_info["emails"] = sorted(account_info["emails"],
                                     key=lambda k: k['primary'],
                                     reverse=True)
 
-    return json.dumps(account_info)
+    return account_info
 
-    # if session.get("current_user"):
-    #     if request.method == "GET":
-    #         user = session["current_user"]
-    #         account_info["username"] = user
-    #         account_info["permissions"] = db_utils.get_user_column_by_username(user, "permissions")
-    #         account_info["first_name"] = "First"
-    #         account_info["last_name"] = "Last"
-    #         account_info["email"] = [{"address": "userX@email1.com", "primary": False}, {"address": "userX@email2.com", "primary": True}]
-    #         account_info["emails"] = sorted(account_info["emails"], key=lambda k: k['primary'], reverse=True)
+# @account_blueprint.route("/test")
+# def account_test(loginname=None):
+#     """account page"""
 
-    #     if request.method == "POST":
-    #         pass
-    #         # username = request.form.get("username")
-    #         # password = request.form.get("password")
+#     username=session["current_user"]
+
+#     account_info = {
+#             'username': username,
+#             'emails': [], #[{"address": "userX@email1.com", "primary": False}, {"address": "userX@email2.com", "primary": True}]
+#             'permissions': None,
+#             'first_name': None,
+#             'last_name': None
+#         }
+
+#     for info in account_info:
+#         if info != "username" or info !="emails":
+#             try:
+#                 # TO DO: change to db.one_or_none()
+#                 account_info[info] = db_utils.get_user_column_by_username(username, info)
+#             except:
+#                 pass
+#         if info == "emails":
+#             user_info_list = models.User.query.filter_by(username=username).all()
+#             account_info["emails"] = [
+#                 {'address': getattr(user_row, "email", None),
+#                 'primary': getattr(user_row, "primary", False)}
+#                 for user_row in user_info_list
+#             ]
+
+#     account_info["emails"] = sorted(account_info["emails"],
+#                                     key=lambda k: k['primary'],
+#                                     reverse=True)
+#     # TO DO:
+#     # GET info
+#     # POST new email
+#     # DELETE email
+#     # PUT update name
+
+#     return json.dumps(account_info)
