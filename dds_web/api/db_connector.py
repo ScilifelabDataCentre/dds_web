d83b8b6c0e2b4a6b3bcc2a3e4bb313fdfdee9a4c
Ina Odén Österbo <ina.oden.osterbo@gmail.com>
2021-09-13 13:12:25 +0200

    incorrect import of utils

--- dds_web/api/db_connector.py
+++ dds_web/api/db_connector.py
@@ -17,7 +17,6 @@ from sqlalchemy.sql import func
 
 # Own modules
 from dds_web import app
-from dds_web import utils
 from dds_web.database import models
 from dds_web import db
 from dds_web.api.dds_decorators import token_required
@@ -30,6 +29,7 @@ from dds_web.api.errors import (
     NoSuchProjectError,
     S3ProjectNotFoundError,
 )
+import dds_web.utils
 
 ###############################################################################
 # CLASSES ########################################################### CLASSES #
@@ -232,7 +232,7 @@ class DBConnector:
             # TODO (ina): put in class
             # change project size
             current_project.size = 0
-            current_project.date_updated = timestamp()
+            current_project.date_updated = dds_web.utils.timestamp()
             db.session.commit()
         except sqlalchemy.exc.SQLAlchemyError as err:
             db.session.rollback()
@@ -281,13 +281,13 @@ class DBConnector:
                             models.Version.time_deleted == None,
                         )
                     ).first()
-                    current_file_version.time_deleted = timestamp()
+                    current_file_version.time_deleted = dds_web.utils.timestamp()
 
                     # Delete file and update project size
                     old_size = x.size_original
                     db.session.delete(x)
                     current_project.size -= old_size
-                current_project.date_updated = timestamp()
+                current_project.date_updated = dds_web.utils.timestamp()
             except sqlalchemy.exc.SQLAlchemyError as err:
                 error = str(err)
             else:
@@ -378,11 +378,11 @@ class DBConnector:
                         models.Version.time_deleted == None,
                     )
                 ).first()
-                current_file_version.time_deleted = timestamp()
+                current_file_version.time_deleted = dds_web.utils.timestamp()
 
                 db.session.delete(file)
                 current_project.size -= old_size
-                current_project.date_updated = timestamp()
+                current_project.date_updated = dds_web.utils.timestamp()
             except sqlalchemy.exc.SQLAlchemyError as err:
                 db.session.rollback()
                 error = str(err)
@@ -438,7 +438,7 @@ class DBConnector:
                     "%Y-%m-%d %H:%M:%S.%f%z",
                 )
                 time_deleted = datetime.datetime.strptime(
-                    v.time_deleted if v.time_deleted else timestamp(),
+                    v.time_deleted if v.time_deleted else dds_web.utils.timestamp(),
                     "%Y-%m-%d %H:%M:%S.%f%z",
                 )
                 file_hours = (time_deleted - time_uploaded).seconds / (60 * 60)
