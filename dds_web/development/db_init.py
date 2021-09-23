042f789f131e97a61aa54f757b4c39dd016799de
Johannes Alneberg <alneberg@kth.se>
2021-09-23 15:26:24 +0200

    Default value for safe_spring_project

--- dds_web/development/db_init.py
+++ dds_web/development/db_init.py
@@ -27,13 +27,13 @@ facilities = [
         public_id="public_facility_id",
         name="Facility 1",
         internal_ref="fac",
-        safespring=current_app.config.get("DDS_SAFE_SPRING_PROJECT"),
+        safespring=current_app.config.get("DDS_SAFE_SPRING_PROJECT", "dds.example.com"),
     ),
     Facility(
         public_id="public_facility_id_2",
         name="Facility 2",
         internal_ref="fac2",
-        safespring=current_app.config.get("DDS_SAFE_SPRING_PROJECT"),
+        safespring=current_app.config.get("DDS_SAFE_SPRING_PROJECT", "dds.example.com"),
     ),
 ]
 
