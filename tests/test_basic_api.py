94914d4bee391352c72c6442e9cff30e8037e9f7
Johannes Alneberg <alneberg@kth.se>
2021-09-27 16:43:27 +0200

    Removed intentionally failing test

--- tests/test_basic_api.py
+++ tests/test_basic_api.py
@@ -10,7 +10,3 @@ def test_token_with_credentials(client):
     credentials = b64encode(b"username:password").decode("utf-8")
     response = client.get("/api/v1/user/token", headers={"Authorization": f"Basic {credentials}"})
     assert response.status == "200 OK"
-
-
-def test_third(client):
-    assert False
