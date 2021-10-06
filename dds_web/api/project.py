2c08f3f065620daa780089a501c8167e446aecb0
Ina Odén Österbo <ina.oden.osterbo@gmail.com>
2021-10-06 11:15:38 +0200

    removed invalid query

--- dds_web/api/project.py
+++ dds_web/api/project.py
@@ -174,9 +174,6 @@ class UserProjects(flask_restful.Resource):
             "Unit Admin",
         ]
 
-        test_query = current_user.project_associations.filter(models.Project)
-        flask.current_app.logger.info(test_query)
-
         try:
 
             # Get info for all projects
