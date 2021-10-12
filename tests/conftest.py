a27351c3375bce1ae2ab8741f1b483695327a2d7
Ina Odén Österbo <ina.oden.osterbo@gmail.com>
2021-10-12 10:15:17 +0200

    add to db

--- tests/conftest.py
+++ tests/conftest.py
@@ -59,8 +59,10 @@ def client():
             # Create all tables
             db.create_all()
             users, units = demo_data()
-            units[0].users.extend([users[1], users[2]])
             db.session.add_all(units)
+            db.session.flush()
+            users[1].unit = units[0]
+            db.session.add_all(users)
             db.session.commit()
 
             try:
