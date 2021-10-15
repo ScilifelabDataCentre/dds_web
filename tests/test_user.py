a31dfa3169bd161b5452caa623f3f3346568645b
Ina Odén Österbo <ina.oden.osterbo@gmail.com>
2021-10-15 15:34:58 +0200

    json to params

--- tests/test_user.py
+++ tests/test_user.py
@@ -12,7 +12,7 @@ def test_add_user_without_credentials(client):
     response = client.post(
         "/api/v1/user/add",
         headers={"Authorization": f"Basic {credentials}"},
-        data=json.dumps(new_user_data),
+        params=new_user_data,
         content_type="application/json",
     )
     assert response.status == "401 UNAUTHORIZED"
@@ -23,89 +23,91 @@ def test_add_user_without_credentials(client):
     assert invited_user is None
 
 
-def test_add_user_with_unitadmin_without_user_data(client):
-    credentials = b64encode(b"unitadmin:password").decode("utf-8")
-    response = client.post(
-        "/api/v1/user/add",
-        headers={"Authorization": f"Basic {credentials}"},
-        data=json.dumps({}),
-        content_type="application/json",
-    )
-    assert response.status == "400 BAD REQUEST"
+# def test_add_user_with_unitadmin_without_user_data(client):
+#     credentials = b64encode(b"unitadmin:password").decode("utf-8")
+#     response = client.post(
+#         "/api/v1/user/add",
+#         headers={"Authorization": f"Basic {credentials}"},
+#         # data=json.dumps({}),
+#         content_type="application/json",
+#     )
+#     assert response.status == "400 BAD REQUEST"
 
 
 def test_add_user_with_unitadmin_without_role(client):
+    new_user_data = {"email": "first_test_email@mailtrap.io"}
     credentials = b64encode(b"unitadmin:password").decode("utf-8")
     response = client.post(
         "/api/v1/user/add",
         headers={"Authorization": f"Basic {credentials}"},
-        data=json.dumps({"email": "first_test_email@mailtrap.io"}),
+        params=new_user_data,
         content_type="application/json",
     )
     assert response.status == "500 SERVER ERROR"
 
 
 def test_add_user_with_unitadmin_with_extraargs(client):
+    new_user_data = {"email": "first_test_email@mailtrap.io", "extra": "extrainfo"}
     credentials = b64encode(b"unitadmin:password").decode("utf-8")
     response = client.post(
         "/api/v1/user/add",
         headers={"Authorization": f"Basic {credentials}"},
-        data=json.dumps({"email": "first_test_email@mailtrap.io", "extra": "extrainfo"}),
+        params=new_user_data,
         content_type="application/json",
     )
     assert response.status == "400 BAD REQUEST"
 
 
 def test_add_user_with_unitadmin_and_invalid_role(client):
-    invite_info = {"email": "first_test_email@mailtrap.io", "role": "Invalid Role"}
+    new_user_data = {"email": "first_test_email@mailtrap.io", "role": "Invalid Role"}
     credentials = b64encode(b"unitadmin:password").decode("utf-8")
 
     response = client.post(
         "/api/v1/user/add",
         headers={"Authorization": f"Basic {credentials}"},
-        data=json.dumps(invite_info),
+        params=new_user_data,
         content_type="application/json",
     )
     assert response.status == "400 BAD REQUEST"
 
     invited_user = (
-        db.session.query(models.Invite).filter_by(email=invite_info["email"]).one_or_none()
+        db.session.query(models.Invite).filter_by(email=new_user_data["email"]).one_or_none()
     )
     assert invited_user is None
 
 
 def test_add_user_with_unitadmin_and_invalid_email(client):
-    invite_info = {"email": "first_test_email", "role": "Researcher"}
+    new_user_data = {"email": "first_test_email", "role": "Researcher"}
     credentials = b64encode(b"unitadmin:password").decode("utf-8")
 
     response = client.post(
         "/api/v1/user/add",
         headers={"Authorization": f"Basic {credentials}"},
-        data=json.dumps(invite_info),
+        params=new_user_data,
         content_type="application/json",
     )
     assert response.status == "400 BAD REQUEST"
 
     invited_user = (
-        db.session.query(models.Invite).filter_by(email=invite_info["email"]).one_or_none()
+        db.session.query(models.Invite).filter_by(email=new_user_data["email"]).one_or_none()
     )
     assert invited_user is None
 
 
 def test_add_user_with_unitadmin(client):
 
-    invite_info = {"email": "first_test_email@mailtrap.io", "role": "Researcher"}
+    invite_infnew_user_datao = {"email": "first_test_email@mailtrap.io", "role": "Researcher"}
     credentials = b64encode(b"unitadmin:password").decode("utf-8")
     response = client.post(
         "/api/v1/user/add",
         headers={"Authorization": f"Basic {credentials}"},
-        data=json.dumps(invite_info),
+        params=new_user_data,
         content_type="application/json",
     )
     assert response.status == "200 OK"
 
     invited_user = (
-        db.session.query(models.Invite).filter_by(email=invite_info["email"]).one_or_none()
+        db.session.query(models.Invite).filter_by(email=new_user_data["email"]).one_or_none()
     )
     assert invited_user
     assert invited_user.email == invite_info["email"]
@@ -113,29 +115,29 @@ def test_add_user_with_unitadmin(client):
 
 
 def test_add_user_existing_email(client):
-    invite_info = {"email": "first_test_email@mailtrap.io", "role": "Researcher"}
+    new_user_data = {"email": "first_test_email@mailtrap.io", "role": "Researcher"}
     credentials = b64encode(b"unitadmin:password").decode("utf-8")
     response = client.post(
         "/api/v1/user/add",
         headers={"Authorization": f"Basic {credentials}"},
-        data=json.dumps(invite_info),
+        params=new_user_data,
         content_type="application/json",
     )
     assert response.status == "400 BAD REQUEST"
 
 
 def test_add_user_with_unitpersonnel_permission_denied(client):
-    invite_info = {"email": "second_test_email@mailtrap.io", "role": "Unit Admin"}
+    new_user_data = {"email": "second_test_email@mailtrap.io", "role": "Unit Admin"}
     credentials = b64encode(b"unituser:password").decode("utf-8")
     response = client.post(
         "/api/v1/user/add",
         headers={"Authorization": f"Basic {credentials}"},
-        data=json.dumps(invite_info),
+        params=new_user_data,
         content_type="application/json",
     )
     assert response.status == "400 PERMISSION DENIED"
 
     invited_user = (
-        db.session.query(models.Invite).filter_by(email=invite_info["email"]).one_or_none()
+        db.session.query(models.Invite).filter_by(email=new_user_data["email"]).one_or_none()
     )
     assert invited_user is None
