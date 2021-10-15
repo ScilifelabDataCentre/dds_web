0d44af5f5a17d9e2a420534a39583b1fe8cc6bca
Ina Odén Österbo <ina.oden.osterbo@gmail.com>
2021-10-15 13:13:31 +0200

    unauth instead of forbidden

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,5 +1,6 @@
 from base64 import b64encode
 import json
+from dds_web import db
 
 new_user_data = {"email": "first_email@mailtrap.io"}
 
@@ -12,4 +13,9 @@ def test_add_user_without_credentials(client):
         data=json.dumps(new_user_data),
         content_type="application/json",
     )
-    assert response.status == "403 FORBIDDEN"
+    assert response.status == "401 UNAUTHORIZED"
+
+    invited_user = (
+        db.session.query(models.Invite).filter_by(email=new_user_data["email"]).one_or_none()
+    )
+    assert invited_user is None
