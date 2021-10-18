94731b7b191d9afde25b79c57a3b884e4f679ff1
Ina Odén Österbo <ina.oden.osterbo@gmail.com>
2021-10-18 13:54:04 +0200

    tests changed to matthias format

--- dds_web/api/user.py
+++ dds_web/api/user.py
@@ -62,7 +62,6 @@ class AddUser(flask_restful.Resource):
         """Create an invite and send email."""
 
         args = flask.request.json
-        flask.current_app.logger.debug(f"Args: {args}")
 
         # Check if email is registered to a user
         try:
@@ -72,10 +71,8 @@ class AddUser(flask_restful.Resource):
 
             # Send invite if the user doesn't exist
             try:
-                flask.current_app.logger.debug("Creating invite...")
                 # Use schema to validate and check args, and create invite row
                 new_invite = marshmallows.InviteUserSchema().load(args)
-                flask.current_app.logger.debug(f"Invite created: {new_invite}")
 
             except sqlalchemy.exc.SQLAlchemyError as sqlerr:
                 raise errors.DatabaseError(message=str(sqlerr))
@@ -97,7 +94,6 @@ class AddUser(flask_restful.Resource):
             # TODO: Format response with marshal with?
             return flask.jsonify({"email": new_invite.email, "message": "Invite successful!"})
 
-        flask.current_app.logger.debug(existing_user)
         # If there is an existing user, add them to project.
         return flask.jsonify({"message": "User exists, should be added to the project."})
 
