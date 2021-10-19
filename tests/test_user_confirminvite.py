b905bc0964220d8e5c25353d97cbc03ceaa0e0ad
Ina Odén Österbo <ina.oden.osterbo@gmail.com>
2021-10-19 09:03:24 +0200

    testing valid token

--- tests/test_user_confirminvite.py
+++ tests/test_user_confirminvite.py
@@ -1,5 +1,16 @@
 import json
 import tests
+import flask
+from dds_web import db
+from dds_web.database import models
+import itsdangerous
+
+
+def get_email_token(email):
+    s = itsdangerous.URLSafeTimedSerializer(flask.current_app.config["SECRET_KEY"])
+    token = s.dumps(email, salt="email-confirm")
+
+    return token
 
 
 def test_no_token(client):
@@ -22,3 +33,19 @@ def test_expired_token(client):
         content_type="application/json",
     )
     assert response.status == "400 BAD REQUEST"
+
+
+def test_valid_token(client):
+    invite = (
+        db.session.query(models.Invite)
+        .filter_by(email="existing_invite_email@mailtrap.io", role="Researcher")
+        .one_or_none()
+    )
+    assert invite
+
+    token = get_email_token(email=invite.email)
+    assert token
+
+    response = client.get(tests.DDSEndpoint.USER_CONFIRM + token, content_type="application/json")
+    assert response.status == "200 OK"
+    assert b"Registration form" in response.data
