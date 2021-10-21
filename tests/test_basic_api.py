337b5c209af2a706874a76219e0a9df7215b5890
Ina Odén Österbo <ina.oden.osterbo@gmail.com>
2021-10-21 12:15:48 +0200

    uncommented the commented tests

--- tests/test_basic_api.py
+++ tests/test_basic_api.py
@@ -1,97 +1,97 @@
-# # IMPORTS ################################################################################ IMPORTS #
+# IMPORTS ################################################################################ IMPORTS #
 
-# # Standard library
-# import flask
-# import http
+# Standard library
+import flask
+import http
 
-# # Installed
-# from jwcrypto import jwk, jws
+# Installed
+from jwcrypto import jwk, jws
 
-# # Own
-# import tests
+# Own
+import tests
 
-# # TESTS #################################################################################### TESTS #
-
-
-# def test_auth_check_statuscode_401_missing_info(client):
-#     """
-#     Test that the auth endpoint returns:
-#     Status code: 401/UNAUTHORIZED
-#     Message: Missing or incorrect credentials
-#     """
-
-#     # No params, no auth
-#     response = client.get(tests.DDSEndpoint.TOKEN)
-#     assert response.status_code == http.HTTPStatus.UNAUTHORIZED
-#     response_json = response.json
-#     assert response_json.get("message")
-#     assert "Missing or incorrect credentials" in response_json.get("message")
+# TESTS #################################################################################### TESTS #
 
 
-# def test_auth_incorrect_username_check_statuscode_401_incorrect_info(client):
-#     """Test that the auth endpoint returns
-#     Status code: 401/UNAUTHORIZED
-#     Message: Missing or incorrect credentials
-#     """
+def test_auth_check_statuscode_401_missing_info(client):
+    """
+    Test that the auth endpoint returns:
+    Status code: 401/UNAUTHORIZED
+    Message: Missing or incorrect credentials
+    """
 
-#     response = client.get(
-#         tests.DDSEndpoint.TOKEN, auth=tests.UserAuth(tests.USER_CREDENTIALS["nouser"]).as_tuple()
-#     )
-#     assert response.status_code == http.HTTPStatus.UNAUTHORIZED
-#     response_json = response.json
-#     assert response_json.get("message")
-#     assert "Missing or incorrect credentials" == response_json.get("message")
-
-
-# def test_auth_incorrect_username_and_password_check_statuscode_401_incorrect_info(client):
-#     """Test that the auth endpoint returns
-#     Status code: 401/UNAUTHORIZED
-#     Message: Missing or incorrect credentials
-#     """
-
-#     response = client.get(
-#         tests.DDSEndpoint.TOKEN,
-#         auth=tests.UserAuth(tests.USER_CREDENTIALS["nopassword"]).as_tuple(),
-#     )
-#     assert response.status_code == http.HTTPStatus.UNAUTHORIZED
-#     response_json = response.json
-#     assert response_json.get("message")
-#     assert "Missing or incorrect credentials" == response_json.get("message")
+    # No params, no auth
+    response = client.get(tests.DDSEndpoint.TOKEN)
+    assert response.status_code == http.HTTPStatus.UNAUTHORIZED
+    response_json = response.json
+    assert response_json.get("message")
+    assert "Missing or incorrect credentials" in response_json.get("message")
 
 
-# def test_auth_incorrect_password_check_statuscode_401_incorrect_info(client):
-#     """Test that the auth endpoint returns
-#     Status code: 401/UNAUTHORIZED
-#     Message: Missing or incorrect credentials
-#     """
+def test_auth_incorrect_username_check_statuscode_401_incorrect_info(client):
+    """Test that the auth endpoint returns
+    Status code: 401/UNAUTHORIZED
+    Message: Missing or incorrect credentials
+    """
 
-#     response = client.get(
-#         tests.DDSEndpoint.TOKEN, auth=tests.UserAuth(tests.USER_CREDENTIALS["wronguser"]).as_tuple()
-#     )
-#     assert response.status_code == http.HTTPStatus.UNAUTHORIZED
-#     response_json = response.json
-#     assert response_json.get("message")
-#     assert "Missing or incorrect credentials" == response_json.get("message")
+    response = client.get(
+        tests.DDSEndpoint.TOKEN, auth=tests.UserAuth(tests.USER_CREDENTIALS["nouser"]).as_tuple()
+    )
+    assert response.status_code == http.HTTPStatus.UNAUTHORIZED
+    response_json = response.json
+    assert response_json.get("message")
+    assert "Missing or incorrect credentials" == response_json.get("message")
 
 
-# def test_auth_correctauth_check_statuscode_200_correct_info(client):
-#     """Test that the auth endpoint returns
-#     Status code: 200/OK
-#     Token: includes the authenticated username
-#     """
+def test_auth_incorrect_username_and_password_check_statuscode_401_incorrect_info(client):
+    """Test that the auth endpoint returns
+    Status code: 401/UNAUTHORIZED
+    Message: Missing or incorrect credentials
+    """
 
-#     response = client.get(
-#         tests.DDSEndpoint.TOKEN,
-#         auth=tests.UserAuth(tests.USER_CREDENTIALS["researchuser"]).as_tuple(),
-#     )
-#     assert response.status_code == http.HTTPStatus.OK
-#     response_json = response.json
-#     assert response_json.get("token")
-#     jwstoken = jws.JWS()
-#     jwstoken.deserialize(response_json.get("token"))
-#     jwstoken.verify(jwk.JWK.from_password(flask.current_app.config.get("SECRET_KEY")))
-#     # extracting JWS token payload before verification will raise a `InvalidJWSOperation` error
-#     payload = jws.json_decode(jwstoken.payload)
-#     assert (
-#         payload.get("sub") == tests.UserAuth(tests.USER_CREDENTIALS["researchuser"]).as_tuple()[0]
-#     )
+    response = client.get(
+        tests.DDSEndpoint.TOKEN,
+        auth=tests.UserAuth(tests.USER_CREDENTIALS["nopassword"]).as_tuple(),
+    )
+    assert response.status_code == http.HTTPStatus.UNAUTHORIZED
+    response_json = response.json
+    assert response_json.get("message")
+    assert "Missing or incorrect credentials" == response_json.get("message")
+
+
+def test_auth_incorrect_password_check_statuscode_401_incorrect_info(client):
+    """Test that the auth endpoint returns
+    Status code: 401/UNAUTHORIZED
+    Message: Missing or incorrect credentials
+    """
+
+    response = client.get(
+        tests.DDSEndpoint.TOKEN, auth=tests.UserAuth(tests.USER_CREDENTIALS["wronguser"]).as_tuple()
+    )
+    assert response.status_code == http.HTTPStatus.UNAUTHORIZED
+    response_json = response.json
+    assert response_json.get("message")
+    assert "Missing or incorrect credentials" == response_json.get("message")
+
+
+def test_auth_correctauth_check_statuscode_200_correct_info(client):
+    """Test that the auth endpoint returns
+    Status code: 200/OK
+    Token: includes the authenticated username
+    """
+
+    response = client.get(
+        tests.DDSEndpoint.TOKEN,
+        auth=tests.UserAuth(tests.USER_CREDENTIALS["researchuser"]).as_tuple(),
+    )
+    assert response.status_code == http.HTTPStatus.OK
+    response_json = response.json
+    assert response_json.get("token")
+    jwstoken = jws.JWS()
+    jwstoken.deserialize(response_json.get("token"))
+    jwstoken.verify(jwk.JWK.from_password(flask.current_app.config.get("SECRET_KEY")))
+    # extracting JWS token payload before verification will raise a `InvalidJWSOperation` error
+    payload = jws.json_decode(jwstoken.payload)
+    assert (
+        payload.get("sub") == tests.UserAuth(tests.USER_CREDENTIALS["researchuser"]).as_tuple()[0]
+    )
--- tests/test_project_creation.py
+++ tests/test_project_creation.py
@@ -1,128 +1,128 @@
-# # IMPORTS ################################################################################ IMPORTS #
+# IMPORTS ################################################################################ IMPORTS #
 
-# # Standard library
-# import http
-# import datetime
-# import json
+# Standard library
+import http
+import datetime
+import json
 
-# # Own
-# from dds_web import db
-# from dds_web.database import models
-# import tests
-
-
-# # CONFIG ################################################################################## CONFIG #
-
-# proj_data = {"pi": "piName", "title": "Test proj", "description": "A longer project description"}
-
-# # TESTS #################################################################################### TESTS #
+# Own
+from dds_web import db
+from dds_web.database import models
+import tests
 
 
-# def test_create_project_without_credentials(client):
-#     response = client.post(
-#         tests.DDSEndpoint.PROJECT_CREATE,
-#         headers=tests.UserAuth(tests.USER_CREDENTIALS["researchuser"]).post_headers(),
-#         data=json.dumps(proj_data),
-#         content_type="application/json",
-#     )
-#     assert response.status_code == http.HTTPStatus.FORBIDDEN
-#     created_proj = (
-#         db.session.query(models.Project)
-#         .filter_by(
-#             created_by="researchuser",
-#             title=proj_data["title"],
-#             pi=proj_data["pi"],
-#             description=proj_data["description"],
-#         )
-#         .one_or_none()
-#     )
-#     assert created_proj is None
+# CONFIG ################################################################################## CONFIG #
+
+proj_data = {"pi": "piName", "title": "Test proj", "description": "A longer project description"}
+
+# TESTS #################################################################################### TESTS #
 
 
-# def test_create_project_with_credentials(client):
-#     time_before_run = datetime.datetime.now()
-#     response = client.post(
-#         tests.DDSEndpoint.PROJECT_CREATE,
-#         headers=tests.UserAuth(tests.USER_CREDENTIALS["unituser"]).post_headers(),
-#         data=json.dumps(proj_data),
-#         content_type="application/json",
-#     )
-#     assert response.status_code == http.HTTPStatus.OK
-#     created_proj = (
-#         db.session.query(models.Project)
-#         .filter_by(
-#             created_by="unituser",
-#             title=proj_data["title"],
-#             pi=proj_data["pi"],
-#             description=proj_data["description"],
-#         )
-#         .one_or_none()
-#     )
-#     assert (
-#         created_proj
-#         and created_proj.date_created > time_before_run
-#         and not created_proj.is_sensitive
-#     )
+def test_create_project_without_credentials(client):
+    response = client.post(
+        tests.DDSEndpoint.PROJECT_CREATE,
+        headers=tests.UserAuth(tests.USER_CREDENTIALS["researchuser"]).post_headers(),
+        data=json.dumps(proj_data),
+        content_type="application/json",
+    )
+    assert response.status_code == http.HTTPStatus.FORBIDDEN
+    created_proj = (
+        db.session.query(models.Project)
+        .filter_by(
+            created_by="researchuser",
+            title=proj_data["title"],
+            pi=proj_data["pi"],
+            description=proj_data["description"],
+        )
+        .one_or_none()
+    )
+    assert created_proj is None
 
 
-# def test_create_project_without_title_description(client):
-#     response = client.post(
-#         tests.DDSEndpoint.PROJECT_CREATE,
-#         headers=tests.UserAuth(tests.USER_CREDENTIALS["unituser"]).post_headers(),
-#         data=json.dumps({"pi": "piName"}),
-#         content_type="application/json",
-#     )
-#     assert response.status_code == http.HTTPStatus.BAD_REQUEST
-#     created_proj = (
-#         db.session.query(models.Project)
-#         .filter_by(
-#             created_by="unituser",
-#             pi=proj_data["pi"],
-#         )
-#         .one_or_none()
-#     )
-#     assert created_proj is None
+def test_create_project_with_credentials(client):
+    time_before_run = datetime.datetime.now()
+    response = client.post(
+        tests.DDSEndpoint.PROJECT_CREATE,
+        headers=tests.UserAuth(tests.USER_CREDENTIALS["unituser"]).post_headers(),
+        data=json.dumps(proj_data),
+        content_type="application/json",
+    )
+    assert response.status_code == http.HTTPStatus.OK
+    created_proj = (
+        db.session.query(models.Project)
+        .filter_by(
+            created_by="unituser",
+            title=proj_data["title"],
+            pi=proj_data["pi"],
+            description=proj_data["description"],
+        )
+        .one_or_none()
+    )
+    assert (
+        created_proj
+        and created_proj.date_created > time_before_run
+        and not created_proj.is_sensitive
+    )
 
 
-# def test_create_project_with_malformed_json(client):
-#     response = client.post(
-#         tests.DDSEndpoint.PROJECT_CREATE,
-#         headers=tests.UserAuth(tests.USER_CREDENTIALS["unituser"]).post_headers(),
-#         data="",
-#         content_type="application/json",
-#     )
-#     assert response.status_code == http.HTTPStatus.BAD_REQUEST
-#     created_proj = (
-#         db.session.query(models.Project)
-#         .filter_by(
-#             created_by="unituser",
-#             title="",
-#             pi="",
-#             description="",
-#         )
-#         .one_or_none()
-#     )
-#     assert created_proj is None
+def test_create_project_without_title_description(client):
+    response = client.post(
+        tests.DDSEndpoint.PROJECT_CREATE,
+        headers=tests.UserAuth(tests.USER_CREDENTIALS["unituser"]).post_headers(),
+        data=json.dumps({"pi": "piName"}),
+        content_type="application/json",
+    )
+    assert response.status_code == http.HTTPStatus.BAD_REQUEST
+    created_proj = (
+        db.session.query(models.Project)
+        .filter_by(
+            created_by="unituser",
+            pi=proj_data["pi"],
+        )
+        .one_or_none()
+    )
+    assert created_proj is None
 
 
-# def test_create_project_sensitive(client):
-#     p_data = proj_data
-#     p_data["sensitive"] = True
-#     response = client.post(
-#         tests.DDSEndpoint.PROJECT_CREATE,
-#         headers=tests.UserAuth(tests.USER_CREDENTIALS["unituser"]).post_headers(),
-#         data=json.dumps(p_data),
-#         content_type="application/json",
-#     )
-#     assert response.status == "200 OK"
-#     created_proj = (
-#         db.session.query(models.Project)
-#         .filter_by(
-#             created_by="unituser",
-#             title=proj_data["title"],
-#             pi=proj_data["pi"],
-#             description=proj_data["description"],
-#         )
-#         .one_or_none()
-#     )
-#     assert created_proj and created_proj.is_sensitive
+def test_create_project_with_malformed_json(client):
+    response = client.post(
+        tests.DDSEndpoint.PROJECT_CREATE,
+        headers=tests.UserAuth(tests.USER_CREDENTIALS["unituser"]).post_headers(),
+        data="",
+        content_type="application/json",
+    )
+    assert response.status_code == http.HTTPStatus.BAD_REQUEST
+    created_proj = (
+        db.session.query(models.Project)
+        .filter_by(
+            created_by="unituser",
+            title="",
+            pi="",
+            description="",
+        )
+        .one_or_none()
+    )
+    assert created_proj is None
+
+
+def test_create_project_sensitive(client):
+    p_data = proj_data
+    p_data["sensitive"] = True
+    response = client.post(
+        tests.DDSEndpoint.PROJECT_CREATE,
+        headers=tests.UserAuth(tests.USER_CREDENTIALS["unituser"]).post_headers(),
+        data=json.dumps(p_data),
+        content_type="application/json",
+    )
+    assert response.status == "200 OK"
+    created_proj = (
+        db.session.query(models.Project)
+        .filter_by(
+            created_by="unituser",
+            title=proj_data["title"],
+            pi=proj_data["pi"],
+            description=proj_data["description"],
+        )
+        .one_or_none()
+    )
+    assert created_proj and created_proj.is_sensitive
