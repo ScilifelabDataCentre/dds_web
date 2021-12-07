3017b8fbed4494ff95bd6a0e19a2e26ef781e3f1
Ina Odén Österbo <ina.oden.osterbo@gmail.com>
2021-12-07 12:04:19 +0100

    remove unused

--- dds_web/__init__.py
+++ dds_web/__init__.py
@@ -25,7 +25,6 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 import werkzeug
 import sqlalchemy
-import flask_sqlalchemy
 
 ####################################################################################################
 # GLOBAL VARIABLES ############################################################## GLOBAL VARIABLES #
