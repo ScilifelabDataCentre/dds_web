e816a8f9b46951add38873b2be6ccf6eaea0bd6d
Johannes Alneberg <alneberg@kth.se>
2021-12-09 11:55:09 +0100

    Added factories for emails and files and added some logging

--- dds_web/development/factories.py
+++ dds_web/development/factories.py
@@ -2,10 +2,18 @@ import factory
 import os
 import itertools
 import random
+import flask
 
 import dds_web.database.models as models
 from dds_web import db
 from dds_web.crypt import key_gen
+
+STATUSES_PER_PROJECT = 5
+FILES_PER_PROJECT = 10
+NR_UNITS = 2
+USERS_PER_UNIT = 5
+PROJECTS_PER_UNIT = 5
+EMAILS_PER_USER = 3
 
 
 class UnitFactory(factory.alchemy.SQLAlchemyModelFactory):
@@ -30,7 +38,7 @@ class UnitFactory(factory.alchemy.SQLAlchemyModelFactory):
             return
         if extracted:
             if isinstance(extracted, int):
-                UnitUserFactory.create_batch(size=extracted, unit=self)
+                UnitUserFactory.create_batch(size=extracted, unit=self, emails=EMAILS_PER_USER)
             else:
                 pass
 
@@ -41,7 +49,11 @@ class UnitFactory(factory.alchemy.SQLAlchemyModelFactory):
         if extracted:
             if isinstance(extracted, int):
                 ProjectFactory.create_batch(
-                    size=extracted, responsible_unit=self, unit_id=self.id, project_statuses=5
+                    size=extracted,
+                    responsible_unit=self,
+                    unit_id=self.id,
+                    project_statuses=STATUSES_PER_PROJECT,
+                    files=FILES_PER_PROJECT,
                 )
             else:
                 pass
@@ -58,6 +70,16 @@ class UnitUserFactory(factory.alchemy.SQLAlchemyModelFactory):
 
     unit = factory.SubFactory(UnitFactory)
 
+    @factory.post_generation
+    def emails(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            if isinstance(extracted, int):
+                EmailFactory.create_batch(size=extracted, user=self)
+            else:
+                pass
+
 
 class ResearchUserFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
@@ -67,6 +89,26 @@ class ResearchUserFactory(factory.alchemy.SQLAlchemyModelFactory):
     username = factory.Sequence(lambda n: f"research_user_{n}")
     password = "password"
     name = factory.Faker("name")
+
+    @factory.post_generation
+    def emails(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            if isinstance(extracted, int):
+                EmailFactory.create_batch(size=extracted, user=self)
+            else:
+                pass
+
+
+class EmailFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = models.Email
+        sqlalchemy_session = db.session
+
+    id = factory.Sequence(lambda n: n)
+    email = factory.Faker("email")
+    primary = factory.Faker("boolean")
 
 
 class ProjectFactory(factory.alchemy.SQLAlchemyModelFactory):
@@ -108,6 +150,16 @@ class ProjectFactory(factory.alchemy.SQLAlchemyModelFactory):
             else:
                 pass
 
+    @factory.post_generation
+    def files(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            if isinstance(extracted, int):
+                FilesFactory.create_batch(size=extracted, project=self)
+            else:
+                pass
+
 
 class ProjectUserFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
@@ -133,13 +185,52 @@ class ProjectStatusesFactory(factory.alchemy.SQLAlchemyModelFactory):
     date_created = factory.Faker("date_time")
 
 
+def random_filesize():
+    """returns a"""
+    nr_bits = random.randint(1, 40)
+    return random.getrandbits(nr_bits)
+
+
+class FilesFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = models.File
+        sqlalchemy_session = db.session
+
+    project = factory.SubFactory(ProjectFactory)
+    name = factory.Faker("file_name")
+    name_in_bucket = factory.Faker("text")
+    subpath = factory.Faker("file_name")
+    size_original = factory.LazyFunction(random_filesize)
+    size_stored = factory.LazyFunction(random_filesize)
+    compressed = factory.Faker("boolean")
+    public_key = factory.Faker("pystr", min_chars=64, max_chars=64)
+    salt = factory.Faker("pystr", min_chars=32, max_chars=32)
+    checksum = factory.Faker("pystr", min_chars=64, max_chars=64)
+    time_latest_download = factory.Faker("date_time")
+    expires = factory.Faker("date_time")
+
+    # versions
+
+
 def create_all():
-    factories = [UnitFactory, UnitUserFactory, ResearchUserFactory, ProjectFactory]
+    factories = [
+        UnitFactory,
+        UnitUserFactory,
+        ResearchUserFactory,
+        EmailFactory,
+        ProjectFactory,
+        ProjectUserFactory,
+        ProjectStatusesFactory,
+        FilesFactory,
+    ]
     for factory_class in factories:
         factory_class.reset_sequence(1)
 
-    r_users = ResearchUserFactory.create_batch(50)
-    UnitFactory.create_batch(3, projects=100, users=10)
+    r_users = ResearchUserFactory.create_batch(50, emails=EMAILS_PER_USER)
+    flask.current_app.logger.info("Created research users")
+    for i in range(NR_UNITS):
+        UnitFactory.create(projects=PROJECTS_PER_UNIT, users=USERS_PER_UNIT)
+        flask.current_app.logger.info(f"Created unit {i}")
 
     for unit in models.Unit.query.all():
         users = models.ResearchUser.query.all()
@@ -150,5 +241,6 @@ def create_all():
         else:
             for project, user in zip(itertools.cycle(projects), users):
                 ProjectUserFactory(researchuser=user, project=project)
+    flask.current_app.logger.info("Created project user associations")
 
     db.session.commit()
