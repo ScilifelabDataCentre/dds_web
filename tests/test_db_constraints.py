472e0ad56b70fcebb5da42780a8308ca6ed5e55b
Johannes Alneberg <alneberg@kth.se>
2021-12-20 16:11:30 +0100

    Test constraints for removing unit

--- tests/test_db_constraints.py
+++ tests/test_db_constraints.py
@@ -18,6 +18,18 @@ from dds_web.database import models
 # Unit ###################################################################################### Unit #
 
 
+def __setup_unit(client):
+    unit = models.Unit.query.filter_by(name="Unit 1").first()
+
+    projects = unit.projects
+    assert projects != []
+
+    unit_users = unit.users
+    assert unit_users != []
+
+    return unit, projects, unit_users
+
+
 def test_delete_unit_row__with_project_and_users(client):
     """
     Unit row deleted when project and users are not deleted.
@@ -26,13 +38,7 @@ def test_delete_unit_row__with_project_and_users(client):
         Need to delete Project and UnitUser (due to inheritance issues) rows first
         Invite rows deleted
     """
-
-    unit = models.Unit.query.filter_by(name="Unit 1").first()
-
-    projects = unit.projects
-    assert projects != []
-    unit_users = unit.users
-    assert unit_users != []
+    unit, _, _ = __setup_unit(client)
 
     db.session.delete(unit)
     with pytest.raises(sqlalchemy.exc.IntegrityError):
@@ -46,48 +52,47 @@ def test_delete_unit_row__with_users(client):
     Error
         Need to delete UnitUsers (due to inheritance issues) rows first
     """
-
-    unit = models.Unit.query.filter_by(name="Unit 1").first()
-
-    projects = unit.projects
-    assert projects != []
-    unit_users = unit.users
-    assert unit_users != []
+    unit, projects, _ = __setup_unit(client)
 
     for project in projects:
         db.session.delete(project)
     db.session.commit()
 
-    # With this removed, the exception does not occur
-    # print(unit.users)
-
-    db.session.delete(unit)
-    with pytest.raises(sqlalchemy.exc.IntegrityError):
-        db.session.commit()
-
-
-def test_delete_unit_row__with_users2(client):
-    """
-    Unit row deleted when users are not deleted.
-
-    Error
-        Need to delete UnitUsers (due to inheritance issues) rows first
-    """
-
-    unit = models.Unit.query.filter_by(name="Unit 1").first()
-
-    projects = unit.projects
-    assert projects != []
-    unit_users = unit.users
-    assert unit_users != []
-
-    for project in projects:
-        db.session.delete(project)
-    db.session.commit()
-
-    # This seems to make the exception raise as it should below
+    # Removing this print statement causes the test to fail on Johannes computer. I don't know why.
     print(unit.users)
 
     db.session.delete(unit)
     with pytest.raises(sqlalchemy.exc.IntegrityError):
         db.session.commit()
+
+
+def test_delete_unit_row(client):
+    """
+    Unit row deleted when projects and users are deleted.
+
+    Invite rows should be deleted
+    """
+
+    unit, projects, unit_users = __setup_unit(client)
+
+    unit_id = unit.id
+    # Make sure unit has some invites
+    invites = unit.invites
+    assert invites != []
+
+    for project in projects:
+        db.session.delete(project)
+
+    for user in unit_users:
+        db.session.delete(user)
+    db.session.commit()
+
+    db.session.delete(unit)
+    db.session.commit()
+
+    unit = models.Unit.query.filter_by(name="Unit 1").one_or_none()
+    assert unit is None
+
+    # Make sure invites have been deleted
+    invites = models.Invite.query.filter_by(unit_id=unit_id).all()
+    assert invites == []
