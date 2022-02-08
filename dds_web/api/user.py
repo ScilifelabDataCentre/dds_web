bfcf4e49b1ddac9b719a1f20ad7a7e74c62454d2
Matthias Zepper <github@mail.matthias-zepper.de>
2022-02-08 21:48:22 +0100

    Basic wireframe to add invitees to projects

--- dds_web/api/user.py
+++ dds_web/api/user.py
@@ -31,7 +31,10 @@ import dds_web.errors as ddserr
 from dds_web.api.db_connector import DBConnector
 from dds_web.api.schemas import project_schemas, user_schemas, token_schemas
 from dds_web.api.dds_decorators import logging_bind_request
-from dds_web.security.project_user_keys import share_project_private_key_with_user
+from dds_web.security.project_user_keys import (
+    share_project_private_key_with_invite,
+    share_project_private_key_with_user,
+)
 from dds_web.security.tokens import encrypted_jwt_token, update_token_with_mfa
 
 # initiate bound logger
@@ -59,7 +62,7 @@ class AddUser(flask_restful.Resource):
 
         if not existing_user:
             # Send invite if the user doesn't exist
-            invite_user_result = self.invite_user(args)
+            invite_user_result = self.invite_user(args, project)
             return invite_user_result, invite_user_result["status"]
 
         else:
@@ -71,7 +74,7 @@ class AddUser(flask_restful.Resource):
 
     @staticmethod
     @logging_bind_request
-    def invite_user(args):
+    def invite_user(args, project):
         """Invite a new user"""
 
         try:
@@ -90,8 +93,11 @@ class AddUser(flask_restful.Resource):
             raise ddserr.InviteError(message=valerr.messages)
 
         # Create URL safe token for invitation link
-        TKEK = "bogus"
+        TKEK = "Bogus"
         # TODO change to real TKEK.
+
+        if project:
+            share_project_private_key_with_invite(auth.current_user(), new_invite, project)
 
         sensitive_content = json.dumps({"invited_email": new_invite.email, "TKEK": TKEK})
 
