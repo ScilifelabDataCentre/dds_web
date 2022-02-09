a854be932a838ffc4861f17ba63f4fe2d0d0f136
Johannes Alneberg <alneberg@kth.se>
2022-02-09 13:30:21 +0100

    Changes requested by @worukan for tests/test_user_confirminvte.py

--- tests/test_user_confirminvite.py
+++ tests/test_user_confirminvite.py
@@ -5,17 +5,16 @@ from dds_web import db
 from dds_web.database import models
 import dds_web.errors as ddserr
 from dds_web.security.tokens import encrypted_jwt_token
-import itsdangerous
-import pytest
 import datetime
 
 
 def get_email_token(email):
-    sensitive_content = json.dumps({"invited_email": email, "TKEK": "BOGUS"})
-
-    token = encrypted_jwt_token(email, sensitive_content, expires_in=datetime.timedelta(hours=24))
-
-    return token
+    return encrypted_jwt_token(
+        username="",
+        sensitive_content="BOGUS",
+        expires_in=datetime.timedelta(hours=24),
+        additional_claims={"inv": email},
+    )
 
 
 def test_no_token(client):
@@ -38,7 +37,15 @@ def test_invalid_token(client):
 def test_expired_token(client):
     response = client.get(
         tests.DDSEndpoint.USER_CONFIRM
-        + "ImZpcnN0X3Rlc3RfZW1haWxAbWFpbHRyYXAuaW8i.YW2HiQ.zT4zcM-yt_5S6NfCn2VoYDQSv_g",
+        + (
+            "eyJhbGciOiJBMjU2S1ciLCJlbmMiOiJBMjU2R0NNIn0.BQvaqAvJHN_2LooUP3oOa_CFOwkrT1cQefXC"
+            "awOlNhD6Y3g-Nn2jyg.PiQs3nSPC-4sWd2D.m0crUKeQNlOFbhybHkXBZx_Akv1H41NyMcyem0p2yWTq"
+            "Ksgxta9k-S0wMWWvFd0Bogq71YvHocH8llVyPCm4ZfrvpiJFH0JslkcfSxFuwVMb-rFWK32PJFV2edbM"
+            "FsirbsJblbNT59rdE24BP07yrGwJlUxL9tLgfcpqidLG5vPsBaDYPQ3WniUUDJE7ymj0eZ23a3FOXCWY"
+            "QG7sZB5fJlnDgHQtjjeT8_22DF811wxqS4QEmn4LH_fv7RPpOwAFIeuERuQ6OTodZfgYVxS1ghbmFvAo"
+            "Vh7u7-tpVzH-W9cGh4rAnNPd1wjIaUDnMBbSwP8cm0pjPOKrG0t8LyTbTzTXTd3kpLLd6DbmPe_EE5lB"
+            "tKEh6slu_4TMi5LrtlGUdUby.Ughc9A6ZHFXjR9i_FSSQBg"
+        ),
         content_type="application/json",
         follow_redirects=True,
     )
