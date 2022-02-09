69cad50a46e61aca2ce1ab43198bd373a4c78c96
Johannes Alneberg <alneberg@kth.se>
2022-02-09 14:07:57 +0100

    Changed to the No token message in tests

--- tests/test_project_listing.py
+++ tests/test_project_listing.py
@@ -29,7 +29,7 @@ def test_list_proj_no_token(client):
     assert response.status_code == http.HTTPStatus.UNAUTHORIZED
     response_json = response.json
     assert response_json.get("message")
-    assert "Invalid token" in response_json.get("message")
+    assert "No token" in response_json.get("message")
 
 
 def test_list_proj_access_granted_ls(client):
@@ -49,7 +49,7 @@ def test_proj_public_no_token(client):
     response = client.get(tests.DDSEndpoint.PROJ_PUBLIC)
     assert response.status_code == http.HTTPStatus.UNAUTHORIZED
     response_json = response.json
-    assert "Invalid token" in response_json.get("message")
+    assert "No token" in response_json.get("message")
 
 
 def test_proj_public_no_project(client):
