7f530f78a9b0d0785fa8dc837592690750fd77ef
Ina Odén Österbo <ina.oden.osterbo@gmail.com>
2022-02-11 13:16:44 +0100

    renamed

---
+++ dds_web/api/db_tools.py
@@ -0,0 +1,41 @@
+"""API DB Connector module."""
+
+####################################################################################################
+# IMPORTS ################################################################################ IMPORTS #
+####################################################################################################
+
+# Standard library
+
+# Installed
+import sqlalchemy
+
+# Own modules
+from dds_web.database import models
+from dds_web import db
+from dds_web.errors import (
+    DatabaseError,
+    UserDeletionError,
+)
+
+####################################################################################################
+# CLASSES ################################################################################ CLASSES #
+####################################################################################################
+
+
+def remove_user_self_deletion_request(user):
+
+    try:
+        request_row = models.DeletionRequest.query.filter(
+            models.DeletionRequest.requester_id == user.username
+        ).one_or_none()
+        if not request_row:
+            raise UserDeletionError("There is no deletion request from this user.")
+
+        email = request_row.email
+        db.session.delete(request_row)
+        db.session.commit()
+    except sqlalchemy.exc.SQLAlchemyError as err:
+        db.session.rollback()
+        raise DatabaseError(message=str(err))
+
+    return email
