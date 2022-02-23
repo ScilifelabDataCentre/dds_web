b7ac3e45a86a6ddd8fc751851548c748ca33ae84
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2022-02-23 12:07:08 +0100

    Use args instead

--- tests/test_bad_requests.py
+++ tests/test_bad_requests.py
@@ -15,119 +15,68 @@ del ENDPOINTS["__module__"]
 del ENDPOINTS["__doc__"]
 
 
-def run_requests(run_func):
+def run_requests(client, args: dict):
+    """Helper function to run requests with endpoints and methods."""
     for req_type in ("DELETE", "GET", "PATCH", "POST", "PUT"):
         for endpoint in ENDPOINTS:
-            run_func(req_type, ENDPOINTS[endpoint])
+            client.open(ENDPOINTS[endpoint], method=req_type, **args)
 
 
 def test_empty_requests(client):
     """Make empty requests to all endpoints."""
-
-    def make_req(req_type, endp):
-        return client.open(
-            endp,
-            method=req_type,
-        )
-
-    run_requests(make_req)
+    args = {}
+    run_requests(client, args)
 
 
 def test_empty_json(client):
     """Make requests with empty json to all endpoints."""
+    args = {"json": {}}
 
-    def make_req(req_type, endp):
-        return client.open(
-            endp,
-            json={},
-            method=req_type,
-        )
-
-    run_requests(make_req)
+    run_requests(client, args)
 
 
 def test_incorrect_json(client):
     """Make requests with incorrect (not what is expected) json to all endpoints."""
+    args = {"json": {"incorrect": True}}
 
-    def make_req(req_type, endp):
-        return client.open(
-            endp,
-            json={"incorrect": True},
-            method=req_type,
-        )
-
-    run_requests(make_req)
+    run_requests(client, args)
 
 
 def test_invalid_json(client):
     """Make requests with invalid json to all endpoints."""
+    args = {"data": "invalid",
+            "content_type": "application/json"}
 
-    def make_req(req_type, endp):
-        return client.open(
-            endp,
-            data="invalid",
-            content_type="application/json",
-            method=req_type,
-        )
-
-    run_requests(make_req)
+    run_requests(client, args)
 
 
 def test_auth_empty_requests(client):
     """Make authenticated empty requests to all endpoints."""
-    token = tests.UserAuth(tests.USER_CREDENTIALS["unitadmin"]).token(client)
+    args = {"headers": tests.UserAuth(tests.USER_CREDENTIALS["unitadmin"]).token(client)}
 
-    def make_req(req_type, endp):
-        return client.open(
-            endp,
-            headers=token,
-            method=req_type,
-        )
-
-    run_requests(make_req)
+    run_requests(client, args)
 
 
 def test_get_auth_empty_json(client):
     """Make authenticated requests with empty json to all endpoints."""
-    token = tests.UserAuth(tests.USER_CREDENTIALS["unitadmin"]).token(client)
+    args = {"headers": tests.UserAuth(tests.USER_CREDENTIALS["unitadmin"]).token(client),
+            "json": {}}
 
-    def make_req(req_type, endp):
-        return client.open(
-            endp,
-            headers=token,
-            json={},
-            method=req_type,
-        )
-
-    run_requests(make_req)
+    run_requests(client, args)
 
 
 def test_get_auth_incorrect_json(client):
     """Make authenticated requests with incorrect (not what is expected) json to all endpoints."""
-    token = tests.UserAuth(tests.USER_CREDENTIALS["unitadmin"]).token(client)
+    args = {"headers": tests.UserAuth(tests.USER_CREDENTIALS["unitadmin"]).token(client),
+            "json": {"incorrect": True}}
 
-    def make_req(req_type, endp):
-        return client.open(
-            endp,
-            headers=token,
-            json={"incorrect": True},
-            method=req_type,
-        )
-
-    run_requests(make_req)
+    run_requests(client, args)
 
 
 def test_get_auth_invalid_json(client):
     """Make authenticated requests with invalid json to all endpoints."""
-    token = tests.UserAuth(tests.USER_CREDENTIALS["unitadmin"]).token(client)
+    args = {"headers": tests.UserAuth(tests.USER_CREDENTIALS["unitadmin"]).token(client),
+            "data": "invalid",
+            "content_type": "application/json"}
 
-    def make_req(req_type, endp):
-        return client.open(
-            endp,
-            headers=token,
-            data="invalid",
-            content_type="application/json",
-            method=req_type,
-        )
-
-    run_requests(make_req)
+    run_requests(client, args)
