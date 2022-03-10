b632465efc71d0b40110d7ddbf8fa3fc4aed5929
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2022-03-10 10:06:09 +0100

    Imports, raise from

--- dds_web/api/dds_decorators.py
+++ dds_web/api/dds_decorators.py
@@ -16,19 +16,16 @@ import sqlalchemy
 import marshmallow
 
 # Own modules
-from dds_web import db, auth
+from dds_web import db
 from dds_web.errors import (
     BucketNotFoundError,
     DatabaseError,
     DDSArgumentError,
     LoggedHTTPException,
-    NoSuchUserError,
-    AccessDeniedError,
     MissingJsonError,
+    S3ConnectionError,
 )
 from dds_web.utils import get_username_or_request_ip
-from dds_web.api.schemas import user_schemas, project_schemas
-from dds_web.database import models
 
 # initiate logging
 action_logger = structlog.getLogger("actions")
@@ -95,7 +92,7 @@ def dbsession(func):
         try:
             db.session.commit()
         except sqlalchemy.exc.SQLAlchemyError as sqlerr:
-            raise DatabaseError(message=str(sqlerr), alt_message="Saving database changes failed.")
+            raise DatabaseError(message=str(sqlerr), alt_message="Saving database changes failed.") from sqlerr
 
         return result
 
@@ -122,9 +119,9 @@ def connect_cloud(func):
                 aws_secret_access_key=self.keys["secret_key"],
             )
         except sqlalchemy.exc.SQLAlchemyError as sqlerr:
-            raise DatabaseError(message=str(sqlerr))
+            raise DatabaseError(message=str(sqlerr)) from sqlerr
         except botocore.client.ClientError as clierr:
-            raise S3ConnectionError(message=str(clierr))
+            raise S3ConnectionError(message=str(clierr)) from sqlerr
 
         return func(self, *args, **kwargs)
 
@@ -139,7 +136,7 @@ def bucket_must_exists(func):
         try:
             self.resource.meta.client.head_bucket(Bucket=self.bucketname)
         except botocore.client.ClientError as err:
-            raise BucketNotFoundError(message=str(err))
+            raise BucketNotFoundError(message=str(err)) from err
 
         return func(self, *args, **kwargs)
 
