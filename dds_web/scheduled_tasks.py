6be7cd694f8c4dcb33ed0ca51fdd5d81ed872b25
Ina <inaod568@users.noreply.github.com>
2022-03-30 08:51:55 +0200

    accessing models now also work

--- dds_web/scheduled_tasks.py
+++ dds_web/scheduled_tasks.py
@@ -10,9 +10,11 @@ scheduler = flask_apscheduler.APScheduler()
 @scheduler.task("interval", id="testtask", seconds=10, misfire_grace_time=1)
 def task1():
     print("task 1 executed", flush=True)
+    from dds_web.database import models
     with scheduler.app.app_context():
         scheduler.app.logger.debug("testing")
-        
+        test = models.Project.query.all()
+        print(test, flush=True)
         # scheduler.
     #     # test = models.Projects.query.all()
     # app.logger.debug("testing")
