4205fe2df34da00bf8ab69d7a8c3c6d8588fef07
valyo <582646+valyo@users.noreply.github.com>
2022-03-30 16:53:24 +0200

    query for projects with current_stus Available

--- dds_web/scheduled_tasks.py
+++ dds_web/scheduled_tasks.py
@@ -1,11 +1,11 @@
 import flask_apscheduler
 import flask
 
-# Apscheduler
+## Apscheduler
 scheduler = flask_apscheduler.APScheduler()
 
 
-@scheduler.task("interval", id="available_to_expired", seconds=50, misfire_grace_time=1)
+@scheduler.task("interval", id="available_to_expired", seconds=5, misfire_grace_time=1)
 def change_status_to_expired():
     # print("Task: Change project status from Available to expired.", flush=True)
     from dds_web.database import models
@@ -14,11 +14,15 @@ def change_status_to_expired():
 
     with scheduler.app.app_context():
         scheduler.app.logger.debug("This means that the app context works!")
-        test = models.ProjectStatuses.query.all()
+        # test = models.Project.query.all()
         # print(test, flush=True)
 
         for project in page_query(
-            models.ProjectStatuses.query.filter(models.ProjectStatuses.deadline >= current_time())
+            # models.ProjectStatuses.query.filter(models.ProjectStatuses.is_active >= current_time())
+            models.Project.query.filter(models.Project.is_active == 1)
         ):
-
-            scheduler.app.logger.debug("Project: %s has stautus %s - Expires: %s", project.project_id, project.status, project.deadline)
+            if "Available" in project.current_status:
+                scheduler.app.logger.debug("Project: %s has status %s - Expires: %s", project.title, project.current_status, project.current_deadline)
+                # scheduler.app.logger.debug(project.id)
+                # scheduler.app.logger.debug(project.current_deadline)
+            
