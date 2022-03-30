6bfff28fa57004b25994d508de91b2113e3d0744
Ina <inaod568@users.noreply.github.com>
2022-03-30 09:01:47 +0200

    fixed scheduled tasks

--- dds_web/scheduled_tasks.py
+++ dds_web/scheduled_tasks.py
@@ -1,21 +1,20 @@
 import flask_apscheduler
 import flask 
 
-# from dds_web.database import models
-
-
 # Apscheduler
 scheduler = flask_apscheduler.APScheduler()
 
-@scheduler.task("interval", id="testtask", seconds=10, misfire_grace_time=1)
-def task1():
-    print("task 1 executed", flush=True)
+@scheduler.task("interval", id="available_to_expired", seconds=10, misfire_grace_time=1)
+def change_status_to_expired():
+    print("Task: Change project status from Available to expired.", flush=True)
     from dds_web.database import models
     # from dds_web.utils import current_time, page_query
+
     with scheduler.app.app_context():
-        scheduler.app.logger.debug("testing")
+        scheduler.app.logger.debug("This means that the app context works!")
         test = models.Project.query.all()
         print(test, flush=True)
+        
         # for project in page_query(
         #     models.ProjectStatuses.query.filter(models.ProjectStatuses.deadline <= current_time())
         # ):
