2b34436ffc3cf5a0eb54c432c3095d67c416bf53
Ina <inaod568@users.noreply.github.com>
2022-03-30 10:26:05 +0200

    don't print everything

--- dds_web/scheduled_tasks.py
+++ dds_web/scheduled_tasks.py
@@ -7,15 +7,15 @@ scheduler = flask_apscheduler.APScheduler()
 
 @scheduler.task("interval", id="available_to_expired", seconds=10, misfire_grace_time=1)
 def change_status_to_expired():
-    print("Task: Change project status from Available to expired.", flush=True)
+    # print("Task: Change project status from Available to expired.", flush=True)
     from dds_web.database import models
 
     # from dds_web.utils import current_time, page_query
 
     with scheduler.app.app_context():
-        scheduler.app.logger.debug("This means that the app context works!")
+        # scheduler.app.logger.debug("This means that the app context works!")
         test = models.Project.query.all()
-        print(test, flush=True)
+        # print(test, flush=True)
 
         # for project in page_query(
         #     models.ProjectStatuses.query.filter(models.ProjectStatuses.deadline <= current_time())
