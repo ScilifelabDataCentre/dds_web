a27f5a59605c251cb89d9a01002d362944b3359b
valyo <582646+valyo@users.noreply.github.com>
2022-04-04 14:46:30 +0200

    added class MOTD with post method (not ready)

--- dds_web/api/unit.py
+++ dds_web/api/unit.py
@@ -8,12 +8,15 @@
 
 # Installed
 import flask_restful
+import flask
 import structlog
 
 # Own modules
-from dds_web import auth
+from dds_web import auth,db
 from dds_web.database import models
 from dds_web.api.dds_decorators import logging_bind_request, handle_db_error
+from dds_web import utils
+import dds_web.errors as ddserr
 
 
 # initiate bound logger
@@ -59,3 +62,19 @@ class AllUnits(flask_restful.Resource):
                 "Contact Email",
             ],
         }
+
+class MOTD(flask_restful.Resource):
+    """Add a new MOTD message."""
+
+    @auth.login_required(role=["Super Admin"])
+    @logging_bind_request
+    @handle_db_error
+    def post(self):
+        """Add a MOTD."""
+
+        motd = flask.request.json
+
+        flask.current_app.logger.debug(motd["message"])
+        new_motd = models.MOTD(message=motd["message"])
+        db.session.add(new_motd)
+        db.session.commit()
