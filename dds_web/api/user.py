463c88c2ca5564d96d72becd4fccc2a7e63ab483
Johannes Alneberg <alneberg@kth.se>
2022-04-07 09:37:53 +0200

    Supply secondfactor method to user after correct credentials

--- dds_web/api/user.py
+++ dds_web/api/user.py
@@ -956,12 +956,14 @@ class EncryptedToken(flask_restful.Resource):
     @basic_auth.login_required
     @logging_bind_request
     def get(self):
+        secondfactor_method = "TOTP" if auth.current_user().totp_enabled else "HOTP"
         return {
             "message": "Please take this token to /user/second_factor to authenticate with MFA!",
             "token": encrypted_jwt_token(
                 username=auth.current_user().username,
                 sensitive_content=flask.request.authorization.get("password"),
             ),
+            "secondfactor_method": secondfactor_method,
         }
 
 
