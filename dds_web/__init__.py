4ae141d162a0208419ca73b216ad1c5c3bf7ece7
Ina <inaod568@users.noreply.github.com>
2022-04-12 13:30:49 +0200

    link

--- dds_web/__init__.py
+++ dds_web/__init__.py
@@ -369,7 +369,11 @@ def create_new_unit(
     days_in_available,
     days_in_expired,
 ):
-    """Create a new unit."""
+    """Create a new unit.
+    
+    Rules for bucket names, which are affected by the public_id at the moment:
+    https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+    """
     from dds_web.database import models
 
     error_message = ""
