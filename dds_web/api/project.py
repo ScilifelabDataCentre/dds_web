a31e5442755b4c1bfa40d58fbd5233d6827a6417
Ina Odén Österbo <35953392+inaod568@users.noreply.github.com>
2022-05-13 14:41:44 +0200

    Update dds_web/api/project.py
    Co-authored-by: valyo <582646+valyo@users.noreply.github.com>

--- dds_web/api/project.py
+++ dds_web/api/project.py
@@ -550,7 +550,7 @@ class RemoveContents(flask_restful.Resource):
         # Delete from cloud
         with ApiS3Connector(project=project) as s3conn:
             try:
-                bucket = s3conn.remove_bucket_contents(delete_bucket=delete_bucket)
+                s3conn.remove_bucket_contents(delete_bucket=delete_bucket)
             except botocore.client.ClientError as err:
                 raise DeletionError(message=str(err), project=project.public_id) from err
 
