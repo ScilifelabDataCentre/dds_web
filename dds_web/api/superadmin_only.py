42f1a6756347ccd902e5dfe7a632067bb1b37d6b
valyo <582646+valyo@users.noreply.github.com>
2022-06-30 12:29:47 +0200

    add column active to motd table

--- dds_web/api/superadmin_only.py
+++ dds_web/api/superadmin_only.py
@@ -81,7 +81,7 @@ class MOTD(flask_restful.Resource):
             raise ddserr.DDSArgumentError(message="No MOTD specified.")
 
         flask.current_app.logger.debug(motd)
-        new_motd = models.MOTD(message=motd, date_created=curr_date)
+        new_motd = models.MOTD(message=motd)
         db.session.add(new_motd)
         db.session.commit()
 
@@ -89,9 +89,46 @@ class MOTD(flask_restful.Resource):
 
     @handle_db_error
     def get(self):
-        """Get the latest MOTD from database."""
-        motd = utils.get_latest_motd()
-        return {"message": motd}
+        """Return list of all active MOTDs to super admin."""
+        active_motds = models.MOTD.query.filter_by(active=True).all()
+        motd_info = [
+            {
+                "MOTD ID": m.id,
+                "Message": m.message,
+                "Created": m.date_created,
+            }
+            for m in active_motds
+        ]
+
+        return {
+            "motds": motd_info,
+            "keys": [
+                "MOTD ID",
+                "Message",
+                "Created",
+            ],
+        }
+
+    @auth.login_required(role=["Super Admin"])
+    @logging_bind_request
+    @json_required
+    @handle_db_error
+    def put(self):
+        """Deactivate MOTDs."""
+
+        json_input = flask.request.json
+        motd_id = json_input.get("motd_id")
+        if not motd_id:
+            raise ddserr.DDSArgumentError(message="No MOTD for deactivation specified.")
+
+        motd_to_deactivate = models.MOTD.query.filter_by(id=motd_id).first()
+        if motd_to_deactivate.active == True:
+            motd_to_deactivate.active = 0
+            db.session.commit()
+
+            return {"message": "The MOTD was successfully deactivated the database."}
+        else:
+            raise ddserr.DDSArgumentError(message=f"MOTD with id {motd_id} is not active.")
 
 
 class FindUser(flask_restful.Resource):
