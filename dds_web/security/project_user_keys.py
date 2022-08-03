b3033510ad7e9270a197929a5277a1f8bd934403
Ina <inaod568@users.noreply.github.com>
2022-08-03 14:30:37 +0200

    black

--- dds_web/security/project_user_keys.py
+++ dds_web/security/project_user_keys.py
@@ -32,7 +32,7 @@ def __derive_key(user, password):
         secret=password.encode(),
         salt=user.kd_salt,
         time_cost=2,
-        memory_cost=16384000, # 16384 worked
+        memory_cost=16384000,  # 16384 worked
         parallelism=8,
         hash_len=32,
         type=argon2.Type.ID,
