8b689b4ca553edcb7bbfb255b1e4061fd1a50805
Ina <inaod568@users.noreply.github.com>
2022-08-04 12:34:36 +0200

    parallelism 8 and memory 4 GiB

--- dds_web/security/project_user_keys.py
+++ dds_web/security/project_user_keys.py
@@ -39,8 +39,8 @@ def __derive_key(user, password):
         secret=password.encode(),
         salt=user.kd_salt,
         time_cost=2,
-        memory_cost=143360,  # 16384 worked
-        parallelism=2,
+        memory_cost=4194304,  # 16384 worked
+        parallelism=8,
         hash_len=32,
         type=argon2.Type.ID,
     )
