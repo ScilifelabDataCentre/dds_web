09c83bea2cc47b65217cbb34b06943fec603a97f
valyo <582646+valyo@users.noreply.github.com>
2022-08-05 10:47:52 +0200

    black

--- dds_web/scheduled_tasks.py
+++ dds_web/scheduled_tasks.py
@@ -214,7 +214,7 @@ def delete_invite():
             scheduler.app.logger.error(f"{invite} not deleted: {error}")
 
 
-#@scheduler.task("cron", id="get_monthly_usage", day='1', hour=0, minute=1)
+# @scheduler.task("cron", id="get_monthly_usage", day='1', hour=0, minute=1)
 @scheduler.task("interval", id="monthly_usage", seconds=150, misfire_grace_time=1)
 def monthly_usage():
     """Get the monthly usage for the units"""
