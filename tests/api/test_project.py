7947f95e7da0da4939f5462289f5b114fac4f355
Ina <inaod568@users.noreply.github.com>
2022-08-11 13:48:17 +0200

    added removal of public key

--- tests/api/test_project.py
+++ tests/api/test_project.py
@@ -16,6 +16,7 @@ import sqlalchemy
 
 # Own
 import dds_web
+from dds_web import db
 from dds_web.errors import BucketNotFoundError, DatabaseError, DeletionError
 import tests
 from tests.test_files_new import project_row, file_in_db, FIRST_NEW_FILE
@@ -1150,7 +1151,10 @@ def test_getpublic_publickey_is_none(module_client, boto3_session):
     assert response.status_code == http.HTTPStatus.OK
     project_id = response.json.get("project_id")
     project = project_row(project_id=project_id)
-
+    
+    project.public_key = None
+    db.session.commit()
+    
     # Get public key
     response = module_client.get(
         tests.DDSEndpoint.PROJ_PUBLIC,
