3a53ddd0de0c9795105e58734dc26d1a404ac862
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2022-10-06 15:49:12 +0200

    Fix keys, black formatting

--- dds_web/api/user.py
+++ dds_web/api/user.py
@@ -1245,15 +1245,17 @@ class Users(flask_restful.Resource):
 
         # Unit Personnel and Unit Admins can list all users within their units
         unit_row = auth.current_user().unit
-        
+
         # Get users in unit
         users_to_return = get_users(unit=unit_row)
-        flask.current_app.logger.error({
-            "users": users_to_return,
-            "unit": unit_row.name,
-            "keys": keys,
-            "empty": not users_to_return,
-        })
+        flask.current_app.logger.error(
+            {
+                "users": users_to_return,
+                "unit": unit_row.name,
+                "keys": keys,
+                "empty": not users_to_return,
+            }
+        )
         return {
             "users": users_to_return,
             "unit": unit_row.name,
@@ -1278,17 +1280,25 @@ class InvitedUsers(flask_restful.Resource):
             raw_invites = models.Invite.query.all()
             raw_hits = []
             for inv in raw_invites:
-                if inv.role == "Researcher" and set(proj.id for proj in inv.projects).intersection(unit_projects):
+                if inv.role == "Researcher" and set(proj.id for proj in inv.projects).intersection(
+                    unit_projects
+                ):
                     raw_hits.append(inv)
                 elif inv.role in ("Unit Admin", "Unit Personnel") and inv.unit == unit:
                     raw_hits.append(inv)
         elif current_user.role == "Researcher":
-            owned_projects = models.ProjectUsers.query.filter_by(user_id=current_user.username).filter_by(owner=1).all()
+            owned_projects = (
+                models.ProjectUsers.query.filter_by(user_id=current_user.username)
+                .filter_by(owner=1)
+                .all()
+            )
             user_projects = set(proj.project_id for proj in owned_projects)
             raw_invites = models.Invite.query.all()
             raw_hits = []
             for inv in raw_invites:
-                if inv.role == "Researcher" and set(proj.id for proj in inv.projects).intersection(user_projects):
+                if inv.role == "Researcher" and set(proj.id for proj in inv.projects).intersection(
+                    user_projects
+                ):
                     raw_hits.append(inv)
         else:
             raw_hits = []
@@ -1297,16 +1307,17 @@ class InvitedUsers(flask_restful.Resource):
         key_order = [key_map["email"], key_map["role"], "Projects", key_map["created_at"]]
         if current_user.role == "Super Admin":
             key_map["unit"] = "Unit"
-            key_order = key_order.insert(1, "Unit")
+            key_order.insert(1, "Unit")
 
         for entry in raw_hits:
             hit = {}
             for key in key_map.keys():
-                hit[key_map[key]] = getattr(entry, key)
+                hit[key_map[key]] = getattr(entry, key) or ""
 
             hit["Projects"] = [project.public_id for project in entry.projects]
             if "Unit" in hit and hit["Unit"]:
                 hit["Unit"] = hit["Unit"].name
+
             hits.append(hit)
-       
+
         return {"invites": hits, "keys": key_order}
