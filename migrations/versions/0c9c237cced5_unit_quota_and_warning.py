6b5180be0338b1f56e0d372ed16198e4ed0a1edd
Ina Odén Österbo <i-oden@users.noreply.github.com>
2023-02-01 08:50:05 +0100

    same row instead

--- migrations/versions/0c9c237cced5_unit_quota_and_warning.py
+++ migrations/versions/0c9c237cced5_unit_quota_and_warning.py
@@ -27,10 +27,8 @@ def upgrade():
     unit_table = sa.sql.table(
         "units", sa.sql.column("quota", mysql.BIGINT), sa.sql.column("warning_level", mysql.FLOAT)
     )
-    # 2. Update column value - set value to 100 TB
-    op.execute(unit_table.update().values(quota=100 * (10**12)))
-    # 3. Update column value - set value to 0.80
-    op.execute(unit_table.update().values(warning_level=0.8))
+    # 2. Update column value - set value to 100 TB and 0.8
+    op.execute(unit_table.update().values(quota=100 * (10**12), warning_level=0.8))
     # ### end Alembic commands ###
 
 
