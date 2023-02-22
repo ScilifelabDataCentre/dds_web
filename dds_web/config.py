d559ba581ccfc066380a5d1de1a4c01e197d97e2
Ina Odén Österbo <35953392+i-oden@users.noreply.github.com>
2023-02-22 12:58:50 +0100

    Update dds_web/config.py
    Co-authored-by: Linus Östberg <linus.ostberg@scilifelab.uu.se>

--- dds_web/config.py
+++ dds_web/config.py
@@ -75,6 +75,6 @@ class Config(object):
 
     REVERSE_PROXY = False  # Behind a reverse proxy, use X_Forwarded-For to get the ip
 
-    # Logging setup
+    # Logging setup; 0 means no log rotation
     LOG_MAX_SIZE = os.environ.get("LOG_MAX_SIZE", 0x100000)
     LOG_BACKUP_COUNT = os.environ.get("LOG_BACKUP_COUNT", 15)
