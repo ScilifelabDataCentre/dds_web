8f463f6f132a2eeb81254e964b24411acb63f3a4
Álvaro Revuelta <46089290+rv0lt@users.noreply.github.com>
2023-09-19 09:56:34 +0200

    Update tests/api/test_user.py
    Co-authored-by: Ina Odén Österbo <35953392+i-oden@users.noreply.github.com>

--- tests/api/test_user.py
+++ tests/api/test_user.py
@@ -346,15 +346,6 @@ def test_invite_user_existing_project_invite_expired(client):
     # Send the invite again and confirm it works
     from tests.api.test_project import mock_sqlalchemyerror
 
-    # simulate database error
-    with unittest.mock.patch("dds_web.db.session.add", mock_sqlalchemyerror):
-        response = client.post(
-            tests.DDSEndpoint.USER_ADD,
-            headers=tests.UserAuth(tests.USER_CREDENTIALS["unitadmin"]).token(client),
-            query_string={"project": project.public_id},
-            json=first_new_user,
-        )
-        assert response.status_code == http.HTTPStatus.INTERNAL_SERVER_ERROR
 
     response = client.post(
         tests.DDSEndpoint.USER_ADD,
