4e0aa632fc4817858efbc38b1624bc0d19c144c5
Álvaro Revuelta <46089290+rv0lt@users.noreply.github.com>
2023-09-20 16:41:50 +0200

    Update dds_web/api/user.py
    Co-authored-by: Ina Odén Österbo <35953392+i-oden@users.noreply.github.com>

--- dds_web/api/user.py
+++ dds_web/api/user.py
@@ -922,7 +922,7 @@ class RemoveUserAssociation(flask_restful.Resource):
 
             if not user_in_project:
                 raise ddserr.NoSuchUserError(
-                    f"The user with email '{user_email}' does not have access to the specified project."
+                    f"The user / invite with email '{user_email}' does not have access to the specified project."
                     " Cannot remove non-existent project access."
                 )
 
