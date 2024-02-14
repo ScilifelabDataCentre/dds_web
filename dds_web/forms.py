3560491b14faa797d850aac60b4f47498517ff39
rv0lt <alvarorevu@gmail.com>
2024-02-14 13:59:49 +0100

    match capitalization

--- dds_web/forms.py
+++ dds_web/forms.py
@@ -68,7 +68,7 @@ class RegistrationForm(flask_wtf.FlaskForm):
     )
 
     policy_checkbox = wtforms.BooleanField(
-        "I have read and agree to the Privacy policy and User agreement documents linked above.",
+        "I have read and agree to the Privacy Policy and User Agreement documents linked above.",
         validators=[
             wtforms.validators.DataRequired(message="Please agree to the conditions."),
         ],
