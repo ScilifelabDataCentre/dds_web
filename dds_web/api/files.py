fadc91c1fc08dc5b6314128924368dcc6dfa0cf7
rv0lt <alvarorevu@gmail.com>
2024-05-07 10:38:57 +0200

    feedback

--- dds_web/api/files.py
+++ dds_web/api/files.py
@@ -305,7 +305,7 @@ class ListFiles(flask_restful.Resource):
                 )
 
             # Check if to return file size
-            show_size = flask.request.args.get("show_size", type=inputs.boolean, default=False)
+            show_size = flask.request.args.get("show_size")
 
             # Check if to get from root or folder
             subpath = "."
