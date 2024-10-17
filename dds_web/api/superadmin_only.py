4e87bd0c2fb09f9fec1d4befc95441ddc064a154
rv0lt <alvarorevu@gmail.com>
2024-10-17 16:11:19 +0200

    fix typo

--- dds_web/api/superadmin_only.py
+++ dds_web/api/superadmin_only.py
@@ -207,7 +207,7 @@ class SendMOTD(flask_restful.Resource):
                 utils.send_email_with_retry(msg=msg, obj=conn)
 
         return_msg = f"MOTD '{motd_id}' has been "
-        if unit_personnel_only:
+        if unit_only:
             return_msg += "sent to unit personnel only."
         else:
             return_msg += "sent to all users."
