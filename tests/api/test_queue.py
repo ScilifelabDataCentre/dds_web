c8d842d5aee89ba0d24a8440664596572f9ce0d7
rv0lt <alvarorevu@gmail.com>
2025-03-06 11:44:44 +0100

    fix test

--- tests/api/test_queue.py
+++ tests/api/test_queue.py
@@ -4,12 +4,9 @@ import sqlalchemy
 import typing
 import pytest
 from unittest.mock import patch, MagicMock
+from rq.exceptions import NoSuchJobError
 
-from tests import DDSEndpoint, DEFAULT_HEADER, UserAuth, USER_CREDENTIALS
-from dds_web.database import models
-from dds_web import db
-from dds_web.errors import InvalidJobIdError, DDSArgumentError
-from dds_web.api.queue import JobInfo
+import tests
 
 
 def test_get_job_info_no_job_id(client):
@@ -20,18 +17,18 @@ def test_get_job_info_no_job_id(client):
     assert response.json["message"] == "No job_id provided"
 
 
-@patch("dds_web.api.queue.Job.fetch")
-def test_get_job_info_invalid_job_id(mock_fetch, client):
+def test_get_job_info_invalid_job_id(client):
     """Test getting job info with invalid job_id"""
-
-    from rq.exceptions import NoSuchJobError
 
     token = tests.UserAuth(tests.USER_CREDENTIALS["unitadmin"]).token(client)
 
-    with patch("dds_web.api.qeue.Job.fetch") as mock_fetch:
+    with patch("dds_web.api.queue.Job.fetch") as mock_fetch:
         mock_fetch.side_effect = NoSuchJobError()
 
-        response = client.get(tests.DDSEndpoint.QUEUE_JOB_INFO, headers=token)
+        response = client.get(
+            tests.DDSEndpoint.QUEUE_JOB_INFO, headers=token, query_string={"job_id": "job_id"}
+        )
+
         assert response.status_code == http.HTTPStatus.BAD_REQUEST
         assert response.json["message"] == "Job ID not found in the Redis Queue"
 
@@ -41,7 +38,7 @@ def test_get_job_info_success(client):
 
     token = tests.UserAuth(tests.USER_CREDENTIALS["unitadmin"]).token(client)
 
-    with patch("dds_web.api.qeue.Job.fetch") as mock_fetch:
+    with patch("dds_web.api.queue.Job.fetch") as mock_fetch:
 
         mock_job = MagicMock()
         mock_job.get_status.return_value.name = "FINISHED"
