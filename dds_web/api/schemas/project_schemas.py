f8f928548e55fd85334f1d58412469f0769e91ec
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-09-05 15:04:45 +0200

    should be message

--- dds_web/api/schemas/project_schemas.py
+++ dds_web/api/schemas/project_schemas.py
@@ -146,7 +146,7 @@ class CreateProjectSchema(marshmallow.Schema):
             )
             if not unit_row:
                 raise ddserr.DatabaseError(
-                    alt_message="Error: Your account is not associated to a unit. Contact the Data Centre.",
+                    message="Error: Your account is not associated to a unit. Contact the Data Centre.",
                 )
 
             if unit_row.counter is None:
