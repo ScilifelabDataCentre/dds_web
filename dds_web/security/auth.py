d9baf9af7cc383e2d6fa7d72207eac181f5d365d
Ina Odén Österbo <i-oden@users.noreply.github.com>
2025-09-12 08:41:15 +0200

    black

--- dds_web/security/auth.py
+++ dds_web/security/auth.py
@@ -402,9 +402,11 @@ def verify_token_signature(token):
 def verify_password(username, password):
     """Verify that user exists and that password is correct."""
     if not username or not password:
-        flask.current_app.logger.warning("No username or password provided. Something wrong. This shouldn't happen.")
+        flask.current_app.logger.warning(
+            "No username or password provided. Something wrong. This shouldn't happen."
+        )
         return None
-    
+
     user = models.User.query.get(username)
 
     if user and user.is_active and user.verify_password(input_password=password):
